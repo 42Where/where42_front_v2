88ba1569fcee95af736aa6f45209a52635634469
bok <102132293+bok000111@users.noreply.github.com>
2023-08-07 17:35:03 +0900

    3 page route (#6)
    * [feat] create policy page
    
    개인정보 악관동의 페이지 생성
    
    * [feat] create login page
    
    초기 로그인 페이지 생성
    
    * [fix] replace pages directory
    
    pages 디렉토리를 ./src/에서 프로젝트 루트 디렉토리로 이동
    
    * [fix] apply tailwind on pages
    
    변경된 pages 디렉토리에 맞춰서 tailwind css가 적용되도록 content 수정

---
+++ pages/login/index.tsx
@@ -0,0 +1,7 @@
+function Login() {
+  return (
+    <h1>"로그인페이지입니다"</h1>
+  );
+}
+
+export default Login;
---
+++ pages/policy/index.tsx
@@ -0,0 +1,7 @@
+function Policy() {
+  return (
+    <h1>"약관동의페이지입니다"</h1>
+  );
+}
+
+export default Policy;
