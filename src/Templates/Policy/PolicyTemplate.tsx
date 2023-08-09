4654ed11cf84cc30193470daf638e3187d9160b4
jbok <jbok@student.32seoul.kr>
2023-08-09 13:35:01 +0900

    [feat] create templete component of Policy
    /policy에서 사용할 템플릿 컴포넌트 구현
    
    header - main - footer 순으로 flex column으로 배치
    
    TODO
    공통으로 사용할 header, footer component 구현 필요

---
+++ src/Templates/Policy/PolicyTemplate.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+import style from './PolicyTemplate.module.css';
+
+interface PolicyTemplateProps {
+  // headerComponent: () => ReactNode; - 추후 구현 필요
+  mainComponent: React.FC;
+  // footerComponent: () => ReactNode; - 추후 구현 필요
+}
+
+const PolicyTemplate: React.FC<PolicyTemplateProps> = ({
+  // headerComponent,
+  mainComponent,
+  // footerComponent,
+}) => {
+  return (
+    <div className={style.policyTemplate}>
+      {/* headerComponent 추후 구현 필요 */}
+      <div className={style.policyMain}>{mainComponent()}</div>
+      {/* footerComponent 추후 구현 필요 */}
+    </div>
+  );
+};
+
+export default PolicyTemplate;
