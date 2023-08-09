51b2f7b42e125a728d776c27b03ad65a0acff90b
jbok <jbok@student.32seoul.kr>
2023-08-09 13:31:57 +0900

    [feat] create main component of policy page
    /policy에서 사용할 메인 컴포넌트 구현
    
    titleText는 h1사용
    title - content를 flex col으로 배치

---
+++ src/Organisms/Policy/PolicyMain.tsx
@@ -0,0 +1,17 @@
+import styles from './PolicyMain.module.css';
+import PolicyContent from '@/Molecules/Policy/PolicyContent';
+
+const PolicyMain: React.FC = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.policyTitle}>
+        <h1 className={styles.policyTitleText}>
+          어디있니 개인정보 이용 동의서
+        </h1>
+      </div>
+      <PolicyContent />
+    </div>
+  );
+};
+
+export default PolicyMain;
