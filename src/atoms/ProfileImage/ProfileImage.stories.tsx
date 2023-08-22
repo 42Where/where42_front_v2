776ffe726196c7c2ddb1d2291dfc7773a52de5f6
bok <102132293+bok000111@users.noreply.github.com>
2023-08-22 03:25:23 +0000

    [fix] apply profile image changes
    컴포넌트의 변경내역을 스토리에 반영하였습니다

--- src/atoms/ProfileImage/ProfileImage.stories.tsx
+++ src/atoms/ProfileImage/ProfileImage.stories.tsx
@@ -6,7 +6,7 @@ import ProfileImage from "./ProfileImage";
  * - 사용자의 프로필사진과 출석여부를 표시하는 컴포넌트입니다.
  * - src가 주어지지 않으면 기본 프로필사진을 표시합니다.
  * - active가 true이면 테두리와 활성화아이콘을 표시합니다.
- * - size가 medium이면 96x96, large이면 128x128 크기로 표시합니다.
+ * - size가 small이면 64*64 medium이면 96*96, large이면 128*128 크기로 표시합니다.
  */
 
 export default {
@@ -18,7 +18,7 @@ export default {
         type: "text",
       },
     },
-    active: {
+    isActive: {
       control: {
         type: "boolean",
       },
@@ -26,7 +26,7 @@ export default {
     size: {
       control: {
         type: "select",
-        options: ["medium", "large"],
+        options: ["small", "medium", "large"],
       },
     },
   },
@@ -37,22 +37,22 @@ export default {
 export const BasicProfileImage: StoryObj<typeof ProfileImage> = {
   args: {
     src: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
-    active: true,
-    size: "large",
+    isActive: true,
+    size: "medium",
   },
 };
 
 export const DefaultProfileImage: StoryObj<typeof ProfileImage> = {
   args: {
-    active: false,
-    size: "large",
+    isActive: false,
+    size: "medium",
   },
 };
 
 export const FailedProfileImage: StoryObj<typeof ProfileImage> = {
   args: {
     src: "asdasd",
-    active: false,
-    size: "large",
+    isActive: false,
+    size: "medium",
   },
 };
