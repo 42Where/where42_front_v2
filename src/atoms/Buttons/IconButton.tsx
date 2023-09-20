091c9b943384cb06b44440d2b95c4a41b3ce34fc
bok <102132293+bok000111@users.noreply.github.com>
2023-09-20 05:49:27 +0000

    [feat] impl IconButton
    아이콘 자체나 아이콘과 텍스트를 같이 사용하는 버튼 구현
    text가 없거나 빈 문자열일경우 아이콘만 버튼으로 사용
    그 외에 경우에는 외곽선이 있는 버튼
    
    todo: 아이콘 컴포넌트 자체를 구현해서 깔끔하게 바꿀수도 있음

---
+++ src/atoms/Buttons/IconButton.tsx
@@ -0,0 +1,100 @@
+import Image from "next/image";
+
+import styles from "./IconButton.module.scss";
+
+interface IconButtonProps {
+  /**
+   * 표시할 아이콘
+   * 이미지 아이콘을 사용할 경우는 string, object
+   * 컴포넌트 아이콘을 사용할 경우는 React.FC<any>
+   */
+  Icon: React.FC<any> | any;
+  /**
+   * 아이콘에서 나타낼 값이 필요할 경우 사용하는 인자
+   * 단순 이미지 아이콘을 사용할 경우는 생략 가능
+   */
+  value?: boolean;
+  /**
+   * size
+   * small/medium/large
+   * default: medium
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * 상위 컴포넌트에서 전달받는 onClick 함수
+   */
+  onClick?: () => void;
+  /**
+   * 버튼에 표시할 텍스트
+   */
+  text?: string;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  Icon,
+  value = false,
+  size = "medium",
+  onClick,
+  text,
+}) => {
+  let IconButtonStyle;
+  let IconButtonTextStyle;
+  let iconSize;
+
+  switch (size) {
+    case "small":
+      IconButtonStyle = styles.iconButton + " " + styles.iconButtonSmall;
+      IconButtonTextStyle =
+        styles.iconButtonText + " " + styles.iconButtonTextSmall;
+      iconSize = 16;
+      break;
+    case "medium":
+      IconButtonStyle = styles.iconButton + " " + styles.iconButtonMedium;
+      IconButtonTextStyle =
+        styles.iconButtonText + " " + styles.iconButtonTextMedium;
+      iconSize = 24;
+      break;
+    case "large":
+      IconButtonStyle = styles.iconButton + " " + styles.iconButtonLarge;
+      IconButtonTextStyle =
+        styles.iconButtonText + " " + styles.iconButtonTextLarge;
+      iconSize = 32;
+      break;
+  }
+
+  switch (text) {
+    case undefined || "":
+      if (typeof Icon === "string" || typeof Icon === "object") {
+        return (
+          <Image
+            src={Icon}
+            width={iconSize}
+            height={iconSize}
+            alt="icon"
+            onClick={onClick}
+          />
+        );
+      } else if (typeof Icon === "function") {
+        return <Icon value={value} size={size} onClick={onClick} />;
+      }
+    default:
+      let IconComponent;
+
+      if (typeof Icon === "string" || typeof Icon === "object") {
+        IconComponent = (
+          <Image src={Icon} width={iconSize} height={iconSize} alt="icon" />
+        );
+      } else if (typeof Icon === "function") {
+        IconComponent = <Icon value={value} size={size} />;
+      }
+
+      return (
+        <div className={IconButtonStyle} onClick={onClick}>
+          {IconComponent}
+          <div className={IconButtonTextStyle}>{text}</div>
+        </div>
+      );
+  }
+};
+
+export default IconButton;
