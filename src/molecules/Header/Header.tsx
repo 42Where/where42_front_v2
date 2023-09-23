291057eccd8747a5152e7097236ec2329148dccb
bok <102132293+bok000111@users.noreply.github.com>
2023-09-23 04:46:03 +0000

    [fix] add fix comment
    나중에 찾기 쉽게 작업할부분들에 TODO 주석 추가했습니다

--- src/molecules/Header/Header.tsx
+++ src/molecules/Header/Header.tsx
@@ -6,14 +6,14 @@ import SignOutIcon from "&/Icons/signOut.svg";
 
 import Image from "next/image";
 // import IconButton from "@/atoms/IconButton/IconButton";
-// IconButton 구현 후 Image들을 IconButton으로 변경
+// TODO: IconButton 구현 후 Image들을 IconButton으로 변경
 
 import styles from "./Header.module.scss";
 
 type HeaderProps = {
   /**
    * 현재 페이지의 url
-   * 실제 환경에서는 router 를 통해 받아오거나 redux/zustand를 통해 로그인 여부를 확인할 예정
+   * TODO: 실제 환경에서는 router 를 통해 받아오거나 redux/zustand를 통해 로그인 여부를 확인할 예정
    * 배포시에는 제거
    */
   url?: "/" | "/main" | "/search" | "/login" | "/signup";
@@ -33,7 +33,7 @@ const Header: React.FC<HeaderProps> = ({ url }) => {
     // 추후 검색창으로 이동이 아니라 플로트를 띄우는 방식으로 변경
   };
   const SignOutClickHandler = () => {
-    // 대충 로그아웃 함수 구현 필요
+    // TODO: 대충 로그아웃 함수 구현 필요
     // 재활용 가능성이 많은 함수이므로 다른곳에서 구현후 import해서 재사용해야할수도 있음
   };
 
