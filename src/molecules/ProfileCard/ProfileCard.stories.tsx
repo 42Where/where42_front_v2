ec183603a3d6b17ddb8b028035c4cedc28cf0a3d
bok <102132293+bok000111@users.noreply.github.com>
2023-09-23 17:00:01 +0000

    [fix] refactoring ProfileCard
    새로 추가한 User와 Size를 props로 받도록 수정
    기존 css를 scss로 리팩토링하면서 컴포넌트 구조 간소화
    scss에 타이포그래피와 scss 변수 적용
    onClick 함수를 인자로 받는것이 아니라 children으로 받도록 수정
    용도가 달랐던 Large 컴포넌트를 일관성있게 변경
    TODO: Large 컴포넌트로 사용했던 컴포넌트를 다른 컴포넌트로 분리해서 구현 필요

--- src/molecules/ProfileCard/ProfileCard.stories.tsx
+++ src/molecules/ProfileCard/ProfileCard.stories.tsx
@@ -1,13 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import ProfileCard from "./ProfileCard";
+import { Size } from "@/atoms/types/enums";
+
+import ProfileCard, { ProfileCardProps } from "./ProfileCard";
+import IconButton from "@/atoms/Buttons/IconButton";
+
+import FunctopnButtonIcon from "&/Icons/functionButton.svg";
 
 /**
  * - 사용자의 프로필사진과 아이디, 위치, 한줄소개를 표시하는 컴포넌트입니다.
+ * - 다른 사용자의 컴포넌트로 사용합니다.
  * - src가 주어지지 않으면 기본 프로필사진을 표시합니다.
  * - active가 true이면 테두리와 활성화아이콘을 표시합니다.
- * - large는 로그인한 사용자 본인의 컴포넌트로 사용하고
- * - medium, small은 다른 사용자의 컴포넌트로 사용합니다.
+ * - 친구가 아닐경우 친구추가 버튼을 표시하고 친구일경우 드롭다운 메뉴를 열 수 있는 버튼을 표시합니다.
  */
 
 export default {
@@ -18,17 +23,46 @@ export default {
 
 export const BasicProfileCard: StoryObj<typeof ProfileCard> = {
   args: {
-    loginId: "test",
-    location: "개포 클러스터 내부",
-    userComment: "안녕하세요",
-    profileImageSrc:
-      "https://cards.pexels.com/photos/220453/pexels-photo-220453.jpeg",
-    size: "medium",
+    user: {
+      login: "lorem",
+      profileImgSrc:
+        "https://cards.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+      location: "개포 클러스터 내부",
+      comment: "안녕하세요",
+      locationUsageAgreement: true,
+      isFriend: true,
+    },
+    size: Size.Medium,
   },
 };
 
-export const DefaultProfileCard: StoryObj<typeof ProfileCard> = {
+export const NotFriendProfileCard: StoryObj<typeof ProfileCard> = {
   args: {
-    loginId: "aaaaa",
+    user: {
+      login: "ipsum",
+      profileImgSrc:
+        "https://cards.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+      location: "개포 클러스터 내부",
+      // comment: "안녕하세요",
+      locationUsageAgreement: false,
+      isFriend: false,
+    },
+    size: Size.Medium,
+  },
+};
+
+export const Mykang: StoryObj<typeof ProfileCard> = {
+  args: {
+    user: {
+      login: "mykang",
+      profileImgSrc:
+        "https://cdn.intra.42.fr/users/7ba3314a458b65fd50c749fd184301da/mykang.jpg",
+      location: "개포",
+      comment: "안녕하세요",
+      locationUsageAgreement: true,
+      isFriend: true,
+    },
+    size: Size.Medium,
+    children: <IconButton Icon={FunctopnButtonIcon} />,
   },
 };
--- src/molecules/ProfileCard/ProfileCard.tsx
+++ src/molecules/ProfileCard/ProfileCard.tsx
@@ -1,159 +1,75 @@
-import { Fragment } from "react";
-import Image from "next/image";
-
-import FunctionButton from "&/Icons/functionButton.svg";
-
+import { Size } from "@/atoms/types/enums";
+import User from "@/atoms/types/User";
 import ProfileImage from "@/atoms/ProfileImage/ProfileImage";
-import cardStyles from "./ProfileCard.module.css";
-import textStyles from "./ProfileText.module.css";
 
-type ProfileCardProps = {
+import cardStyles from "./ProfileCard.module.scss";
+import textStyles from "./ProfileText.module.scss";
+
+export type ProfileCardProps = {
   /**
-   * 사용자의 아이디입니다.
+   * 사용자 정보입니다.
    */
-  loginId: string;
-  /**
-   * 사용자의 위치입니다.
-   */
-  location?: string;
-  /**
-   * 사용자의 한줄소개입니다.
-   */
-  userComment?: string;
-  /**
-   * 프로필사진의 URL입니다.
-   */
-  profileImageSrc?: string;
+  user: User;
   /**
    * 컴포넌트의 크기입니다.
-   * large는 본인의 프로필카드로 사용합니다.
-   *  - 추가기능이 많이 필요할경우에 본인 프로필은 별도 컴포넌트로 분리해야할수도 있음.
-   * medium/small은 다른 사용자의 프로필카드로 사용합니다.
    */
-  size?: "small" | "medium" | "large";
+  size?: Size;
   /**
-   * 사용자의 위치 정보 이용 동의 여부입니다.
-   * 지금은 사용하지 않지만 동의 여부에 따라서 퇴근/위치 정보 없음 등을 표시하는 정책 결정 필요.
-   * TODO? 동의를 하지않아 상세 위치를 제공할수 없을때 안내하는 문구나 아이콘을 추가하면 좋을수도 있음.
+   * 기능버튼에 사용할 아이콘 버튼입니다
    */
-  isLocationUseAgreed?: boolean;
-  /**
-   * 프로필사진을 클릭했을 때 실행할 함수입니다.
-   */
-  profileImageOnClick?: () => void;
-  /**
-   * 기능버튼을 클릭했을 때 실행할 함수입니다.
-   * 추후에 본인일경우 설정 아이콘을 표시하거나 모달을 띄우는 등의 기능이 필요할 수 있음.
-   */
-  functionButtonOnClick?: () => void;
+  children?: React.ReactNode;
   // api 연동후에는 api 형식에 맞게 props를 수정해야함.
 };
 
+/**
+ * 다른 사용자의 프로필 컴포넌트로 사용합니다.
+ * TODO: useMemo를 사용하여 불필요한 렌더링을 방지하도록 최적화가 필요합니다.
+ */
 const ProfileCard: React.FC<ProfileCardProps> = ({
-  loginId,
-  location,
-  userComment,
-  profileImageSrc,
-  size = "medium",
-  isLocationUseAgreed = false,
-  profileImageOnClick,
-  functionButtonOnClick,
+  user,
+  size = Size.Medium,
+  children,
 }) => {
-  const profileCardClassName = `${cardStyles["profile-card"]} ${
-    cardStyles["profile-card--" + size]
+  const profileCardClassName = `${cardStyles["profilecard"]} ${
+    cardStyles["profilecard--" + size]
   }`;
-  const profileCardContentLayoutClassName = `${
-    cardStyles["profile-card-content-layout"]
-  } ${cardStyles["profile-card-content-layout--" + size]}`;
+  const profileCardContentClassName = `${cardStyles["profilecard__content"]} ${
+    cardStyles["profilecard__content--" + size]
+  }`;
 
   return (
     <div className={profileCardClassName}>
-      <div className={profileCardContentLayoutClassName}>
-        <ProfileImage
-          loginId={loginId}
-          src={profileImageSrc}
-          isActive={location !== undefined ? true : false}
-          size={size}
-          onClick={profileImageOnClick}
-        />
-        <ProfileText
-          loginId={loginId}
-          location={location}
-          userComment={userComment}
-          size={size}
-          isLocationUseAgreed={isLocationUseAgreed}
-        />
+      <div className={profileCardContentClassName}>
+        <ProfileImage user={user} size={size} />
+        <ProfileText user={user} size={size} />
       </div>
-      <Image
-        src={FunctionButton}
-        alt="functionButton"
-        onClick={functionButtonOnClick}
-      />
+      {children}
     </div>
   );
 };
 
 const ProfileText: React.FC<ProfileCardProps> = ({
-  loginId,
-  location,
-  userComment,
-  size = "medium",
-  isLocationUseAgreed = false,
+  user: { login, location, comment },
+  size,
 }) => {
-  const profileCardTextClassName = `${textStyles["profile-text"]} ${
-    textStyles["profile-text--" + size]
+  const profileTextClassName = `${textStyles["profiletext"]} ${
+    textStyles["profiletext--" + size]
   }`;
-  const profileCardLoginIdStyleClassName = `${
-    textStyles["profile-text-login-id-style"]
-  } ${textStyles["profile-text-login-id-style--" + size]}`;
-
-  let loginIdClassName;
-  let locationClassName;
-  let userCommentClassName;
-  // 타이포그래피 적용 후 글로벌 타이포그래피 사용 필요
-
-  switch (size) {
-    case "large":
-      loginIdClassName = textStyles["profile-text-h5"];
-      locationClassName = textStyles["profile-text-h4"];
-      userCommentClassName = textStyles["profile-text-body3"];
-      break;
-    case "medium":
-      loginIdClassName = textStyles["profile-text-h5"];
-      locationClassName = textStyles["profile-text-h5"];
-      userCommentClassName = textStyles["profile-text-small"];
-      break;
-    case "small":
-      loginIdClassName = textStyles["profile-text-h6"];
-      locationClassName = textStyles["profile-text-h6"];
-      userCommentClassName = textStyles["profile-text-small2"];
-      break;
-  }
+  const loginClassName = `${textStyles["profiletext__login"]} ${
+    textStyles["profiletext__login--" + size]
+  }`;
+  const locationClassName = `${textStyles["profiletext__location"]} ${
+    textStyles["profiletext__location--" + size]
+  }`;
+  const commentClassName = `${textStyles["profiletext__comment"]} ${
+    textStyles["profiletext__comment--" + size]
+  }`;
 
   return (
-    <div className={profileCardTextClassName}>
-      <div className={profileCardLoginIdStyleClassName}>
-        <span className={loginIdClassName}>{loginId}</span>
-      </div>
-      {size === "large" ? (
-        <div className={textStyles["profile-text-large-layout"]}>
-          <div className={textStyles["profile-text-location-style"]}>
-            <div className={locationClassName}>{location ?? "퇴근"}</div>
-          </div>
-          <div className={textStyles["profile-text-comment-style"]}>
-            <div className={userCommentClassName}>{userComment}</div>
-          </div>
-        </div>
-      ) : (
-        <Fragment>
-          <div className={textStyles["profile-text-location-style"]}>
-            <div className={locationClassName}>{location ?? "퇴근"}</div>
-          </div>
-          <div className={textStyles["profile-text-comment-style"]}>
-            <div className={userCommentClassName}>{userComment}</div>
-          </div>
-        </Fragment>
-      )}
+    <div className={profileTextClassName}>
+      <div className={loginClassName}>{login}</div>
+      <div className={locationClassName}>{location ?? "퇴근"}</div>
+      <div className={commentClassName}>{comment}</div>
     </div>
   );
 };
