4199450d3cfdc9931a0650948704afa62bd64f40
bok <102132293+bok000111@users.noreply.github.com>
2023-09-23 14:46:32 +0900

    40 icon buttons (#41)
    * [fix] fix typo
    
    * [feat] impl useToggle
    
    checkbox 등에서 사용할 useToggle 커스텀 훅 구현
    
    * [fix] fix LoginMain scss
    
    매번 mixin을 수정하는게 아니라 바로 가져다 쓰도록 수정
    
    * [feat] light font 추가
    
    구현되어있지 않던 light font class들 구현
    
    * [fix] fix storys directory
    
    Components로 되어있던 경로를 Atoms로 변경
    
    * [feat] add checkbox image
    
    checkbox에서 사용할 svg파일들 추가
    
    * [fix] delete default stories
    
    storybook 설치시에 기본으로 포함되어있는 story들 삭제
    
    * [fix, feat] move icons directory, rename icons
    
    복잡했던 icon 디렉토리를 public/Icons/~로 이동
    아이콘 파일들의 이름을 카멜케이스로 통일
    기존 컴포넌트에 수정된 파일 경로 적용
    
    * [feat] impl checkbox
    
    IconButton에서 사용할 Checkbox 컴포넌트 구현
    todo: hover 기능 구현 필요할수도 있음
    
    * [feat] impl IconButton
    
    아이콘 자체나 아이콘과 텍스트를 같이 사용하는 버튼 구현
    text가 없거나 빈 문자열일경우 아이콘만 버튼으로 사용
    그 외에 경우에는 외곽선이 있는 버튼
    
    todo: 아이콘 컴포넌트 자체를 구현해서 깔끔하게 바꿀수도 있음
    
    * [fix] rename scss class name
    
    bem 네이밍을 따르지 않은 css 클래스명을 수정하고 사이즈별로 일관성있게 패딩 수정
    
    * [fix] refactoring useToggle
    
    다른 컴포넌트에서처럼 const 함수 객체를 선언하고 export하는 방식으로 선언을 변경하고
    의미를 명확하게 하기위해 value, v등의 변수를 state로 변경하였습니다

---
+++ src/atoms/Hooks/useToggle/useToggle.tsx
@@ -0,0 +1,9 @@
+import { useCallback, useState } from "react";
+
+const useToggle = (initialState: boolean) => {
+  const [state, setState] = useState(initialState);
+  const toggle = useCallback(() => setState((prevState) => !prevState), []);
+  return [state, toggle];
+};
+
+export default useToggle;
