92fc61f382b24205c65fc93ed589570036a1976c
bok <102132293+bok000111@users.noreply.github.com>
2023-10-03 15:14:08 +0900

    49 global type scss (#52)
    * [feat] add global type
    
    프로젝트 전역에서 사용할 타입 추가
    
    * [fix] rename scss
    
    일관성있게 파일명 fonts로 수정
    내부 변수명 color-이름-n으로 수정
    이미 사용중인 파일에 변경사항 적용
    
    * [fix] replace enum to type
    
    Size를 enum이 아니라 유니온 타입으로 변경

---
+++ src/types/User.ts
@@ -0,0 +1,45 @@
+type User = {
+  /**
+   * 인트라/백엔드에서 사용하는 고유 id입니다.
+   */
+  id?: number; // 사용하지 않을수도 있음
+  /**
+   * 사용자의 인트라 아이디입니다.
+   */
+  login: string;
+  /**
+   * 프로필사진의 URL입니다.
+   */
+  profileImgSrc?: string;
+  /**
+   * 사용자의 위치입니다.
+   */
+  location?: string;
+  /**
+   * 사용자의 상태메시지입니다.
+   */
+  comment: string;
+  /**
+   * 사용자의 위치 정보 이용 동의 여부입니다.
+   */
+  locationUsageAgreement: boolean;
+  /**
+   * 사용자의 위치 정보 이용 동의 날자입니다.
+   */
+  agreedAt?: Date; // 사용하지 않을수도 있음
+  /**
+   * 사용자의 최근 출석 날자입니다.
+   */
+  updatedAt?: Date; // 사용하지 않을수도 있음
+  /**
+   * 나와 사용자의 관계입니다.
+   */
+  isFriend?: boolean; // 사용하지 않을수도 있음
+  /**
+   * 사용자의 블랙홀 여부입니다.
+   */
+  blackhole?: boolean; // 사용하지 않을수도 있음
+  // 백엔드 api 구현에 따라서 로그인, 프로필사진을 제외한 정보는 다른 타입으로 분리되어 Optional이 될 수 있음
+};
+
+export default User;
