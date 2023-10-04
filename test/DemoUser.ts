c2c638d2f33c831536b23f036bcdb430d787a98d
bok <102132293+bok000111@users.noreply.github.com>
2023-10-04 07:01:56 +0000

    [feat] add demo user
    테스트용 User객체 생성

---
+++ test/DemoUser.ts
@@ -0,0 +1,12 @@
+import User from "@/types/User";
+
+const DemoUser: User = {
+  login: "lorem",
+  profileImgSrc:
+    "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
+  location: "개포 어딘가",
+  comment: "프론트하지마세요체질이라는게바",
+  locationUsageAgreement: true,
+};
+
+export default DemoUser;
