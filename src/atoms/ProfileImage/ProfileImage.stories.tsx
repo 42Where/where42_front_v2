7cad772d547cde109d62be1393e5d89fe12557a3
bok <102132293+bok000111@users.noreply.github.com>
2023-10-04 07:05:08 +0000

    [fix] refactoring profile image
    css파일 scss로 변경
    인자로 User 객체를 사용하도록 변경
    clickHandler 추가 구현
    디자인을 테두리만 표시하는것으로 변경하고 scss 합침

--- src/atoms/ProfileImage/ProfileImage.stories.tsx
+++ src/atoms/ProfileImage/ProfileImage.stories.tsx
@@ -1,58 +1,46 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import User from "@/types/User";
+import { Size } from "@/types/enums";
+
+import DemoUser from "@/../test/DemoUser";
+
 import ProfileImage from "./ProfileImage";
 
 /**
  * - 사용자의 프로필사진과 출석여부를 표시하는 컴포넌트입니다.
  * - src가 주어지지 않으면 기본 프로필사진을 표시합니다.
- * - active가 true이면 테두리와 활성화아이콘을 표시합니다.
+ * - active가 true이면 테두리를 표시합니다.
  * - size가 small이면 64*64 medium이면 96*96, large이면 128*128 크기로 표시합니다.
  */
 
 export default {
   title: "Atoms/ProfileImage",
   component: ProfileImage,
-  argTypes: {
-    src: {
-      control: {
-        type: "text",
-      },
-    },
-    isActive: {
-      control: {
-        type: "boolean",
-      },
-    },
-    size: {
-      control: {
-        type: "select",
-        options: ["small", "medium", "large"],
-      },
-    },
-  },
   tags: ["autodocs"],
 } as Meta;
 
-// const Template: Story = (args) => <ProfileImage {...args} />;
 export const BasicProfileImage: StoryObj<typeof ProfileImage> = {
   args: {
-    src: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
-    isActive: true,
+    user: DemoUser,
     size: "medium",
   },
 };
 
 export const DefaultProfileImage: StoryObj<typeof ProfileImage> = {
   args: {
-    isActive: false,
+    user: { ...DemoUser, profileImgSrc: undefined },
     size: "medium",
   },
 };
 
-export const FailedProfileImage: StoryObj<typeof ProfileImage> = {
+export const AbsentProfileImage: StoryObj<typeof ProfileImage> = {
   args: {
-    src: "asdasd",
-    isActive: false,
+    user: {
+      ...DemoUser,
+      profileImgSrc: "https://www.google.com",
+      location: undefined,
+    },
     size: "medium",
   },
 };
--- src/atoms/ProfileImage/ProfileImage.tsx
+++ src/atoms/ProfileImage/ProfileImage.tsx
@@ -1,91 +1,54 @@
 import { useState } from "react";
 import Image from "next/image";
 
-import imageStyles from "./ProfileImage.module.css";
-import indicatorStyles from "./ProfileImageActiveIndicator.module.css";
-
 import DefaultProfileImage from "&/Common/defaultProfileImageLarge.svg";
+import User from "@/types/User";
+import { Size } from "@/types/enums";
+
+import styles from "./ProfileImage.module.scss";
 
 type ProfileImageProps = {
-  /**
-   * 사용자의 아이디입니다.
-   * Imaged의 alt로 사용됩니다. 인자가 없을경우 "ProfileImage"가 사용됩니다.
-   */
-  loginId?: string;
-  /**
-   * 프로필사진의 URL입니다.
-   * 인자가 들어오지 않거나 이미지 로딩에 실패할경우 기본 이미지파일을 사용합니다.
-   */
-  src?: string;
-  /**
-   * 사용자의 출석여부를 의미합니다.
-   * 출석했을경우 테두리와 활성화 아이콘이 표시됩니다.
-   */
-  isActive?: boolean;
-  /**
-   * 프로필사진의 크기입니다.
-   * small: 64px, medium: 96px, large: 128px
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * 프로필사진을 클릭했을때 실행되는 함수입니다.
-   */
+  user: User;
+  size: Size;
   onClick?: () => void;
 };
 
 const ProfileImage: React.FC<ProfileImageProps> = ({
-  loginId = "ProfileImage",
-  src = DefaultProfileImage,
-  isActive = false,
-  size = "medium",
+  user: { login, profileImgSrc = DefaultProfileImage, location },
+  size,
   onClick = () => {},
 }) => {
-  const [imageSrc, setImageSrc] = useState(src);
+  const [imageSrc, setImageSrc] = useState(profileImgSrc);
+
+  const clickHandler = (event: React.MouseEvent) => {
+    event.preventDefault();
+    onClick();
+  };
 
   const imageErrorHandler = () => {
     setImageSrc(DefaultProfileImage);
   };
 
-  const mode = isActive ? "active" : "inactive";
-  let imageSize;
+  const imageSize = size === "small" ? 64 : size === "medium" ? 96 : 128;
 
-  switch (size) {
-    case "large":
-      imageSize = 128;
-      break;
-    case "medium":
-      imageSize = 96;
-      break;
-    case "small":
-      imageSize = 64;
-      break;
-    default:
-      imageSize = 96;
-      break;
-  }
-
-  const imageSizeClassName = imageStyles[`profile-image--${size}`];
-  const indicatorClassName = indicatorStyles[`active-indicator--${mode}`];
-  const indicatorBorderClassName = `${
-    indicatorStyles["active-indicator__border"]
-  } ${indicatorStyles[`active-indicator__border--${size}`]}`;
-  const indicatorDotClassName = `${indicatorStyles["active-indicator__dot"]} ${
-    indicatorStyles[`active-indicator__dot--${size}`]
-  }`;
+  const ProfileImageClassName =
+    styles["profile-image"] + " " + styles[`profile-image--${size}`];
+  const indicatorClassName =
+    styles["profile-image__indicator"] +
+    " " +
+    styles[`profile-image__indicator--${size}`];
+  const ImageClassName = styles["profile-image__image"];
 
   return (
-    <div className={imageSizeClassName} onClick={onClick}>
-      <div className={indicatorClassName}>
-        <div className={indicatorBorderClassName}>
-          <div className={indicatorDotClassName} />
-        </div>
-      </div>
+    <div className={ProfileImageClassName} onClick={clickHandler}>
+      {location ? <div className={indicatorClassName} /> : null}
       <Image
-        className={imageStyles["profile-image__image"]}
+        className={ImageClassName}
         src={imageSrc}
         width={imageSize}
         height={imageSize}
-        alt={loginId}
+        alt={`${login}'s profile image`}
+        placeholder="empty"
         onError={imageErrorHandler}
       />
     </div>
