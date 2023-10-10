339acf515f07c98fb188610f435c9aab68e4b9d4
bok <102132293+bok000111@users.noreply.github.com>
2023-10-10 08:15:51 +0000

    [feat] add dropdown component
    최상위 컴포넌트에 드롭다운 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "%/globals.css";
 import type { AppProps } from "next/app";
 import Modal from "@/atoms/Modal/Modal";
+import DropdownMenu from "@/atoms/DropdownMenu/DropdownMenu";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Modal />
+      <DropdownMenu />
       <Component {...pageProps} />
     </>
   );
