9b814d45688953f8ef88cfe55f4b7a4c1b875247
bok <102132293+bok000111@users.noreply.github.com>
2023-10-11 05:33:57 +0000

    [feat] add Terms modal test
    중요한 모달 테스트를 약관동의 모달창 테스트로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import IconTextButton from "@/atoms/buttons/IconButton/IconTextButton";
 import IconButton from "@/atoms/buttons/IconButton/IconButton";
 
 import XIcon from "&/Icons/cross.svg";
+import Terms from "@/atoms/Terms/Terms";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,17 +29,20 @@ export default function Home() {
           onClick={(e: React.MouseEvent) => {
             e.preventDefault();
             openImportantModal(
-              <IconTextButton
-                text="바깥이 아니라 안쪽을 눌러야 닫힙니다"
-                size="medium"
-                onClick={closeModal}
-              >
-                <IconButton Icon={XIcon} size="medium" />
-              </IconTextButton>
+              <Terms
+                accept={() => {
+                  console.log("accept");
+                  closeModal();
+                }}
+                reject={() => {
+                  console.log("reject");
+                  closeModal();
+                }}
+              />
             );
           }}
         >
-          중요한 모달창 띄우기
+          약관동의 모달창 띄우기
         </button>
       </section>
     </main>
