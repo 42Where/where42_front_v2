43bd00a686c21d21e5653d45ed27ac723b48f7eb
bok000111 <102132293+bok000111@users.noreply.github.com>
2023-10-24 13:54:56 +0900

    46 reimplement terms component (#63)
    * [feat] add term object
    
    * [feat] impl Terms component
    
    모달창 안에서 사용할 약관동의 컴포넌트 구현
    
    * [fix] fix modal scss
    
    모달창 최대크기 제한
    불필요한 요소 제거
    
    * [feat] add Terms modal test
    
    중요한 모달 테스트를 약관동의 모달창 테스트로 변경
    
    * [fix] delete deprecated Policy components

--- pages/policy/index.tsx
+++
@@ -1,8 +0,0 @@
-import PolicyMain from "@/domains/Policy/organisms/PolicyMain";
-import PolicyTemplate from "@/domains/Policy/templates/PolicyTemplate";
-
-function Policy() {
-  return <PolicyTemplate mainComponent={PolicyMain} />;
-}
-
-export default Policy;
---
+++ src/atoms/Terms/Terms.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+import TermData from "&/terms";
+
+import styles from "./Terms.module.scss";
+import Button from "../buttons/Button/Button";
+
+type TermsProps = {
+  accept: () => void;
+  reject: () => void;
+};
+
+/**
+ * @description
+ * 약관 동의 컴포넌트
+ * @param accept 동의 버튼 클릭 시 실행되는 함수
+ * @param reject 거절 버튼 클릭 시 실행되는 함수
+ * @returns 약관 동의 컴포넌트
+ * TODO: 버튼없이 보여주기만 할 수 있도록 수정해야할수도 있음
+ */
+const Terms: React.FC<TermsProps> = ({ accept, reject }) => {
+  return (
+    <div className={styles.terms}>
+      <div className={styles.terms__title}>{TermData.title}</div>
+      <div className={styles.terms__description}>{TermData.description}</div>
+      <div className={styles.terms__body}>
+        <div className={styles.terms__content}>
+          <div className={styles.terms__title}>
+            {TermData.content.purpose.title}
+          </div>
+          <div className={styles.terms__description}>
+            {TermData.content.purpose.content.join(", ")}
+          </div>
+        </div>
+        <div className={styles.terms__content}>
+          <div className={styles.terms__title}>
+            {TermData.content.collectInfo.title}
+          </div>
+          <div className={styles.terms__description}>
+            {TermData.content.collectInfo.content.join(", ")}
+          </div>
+        </div>
+        <div className={styles.terms__content}>
+          <div className={styles.terms__title}>
+            {TermData.content.retentionPeriod.title}
+          </div>
+          <div className={styles.terms__description}>
+            {TermData.content.retentionPeriod.content.join(" - ")}
+          </div>
+        </div>
+        <div className={styles.terms__content}>
+          <div className={styles.terms__title}>
+            {TermData.content.refusalAndConsequences.title}
+          </div>
+          <div className={styles.terms__description}>
+            {TermData.content.refusalAndConsequences.content.join(" ")}
+          </div>
+        </div>
+      </div>
+      <div className={styles.terms__buttonbox}>
+        <Button onClick={reject}>거절</Button>
+        <Button onClick={accept}>동의</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Terms;
