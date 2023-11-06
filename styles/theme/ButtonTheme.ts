1994b70d57dcb5815b0c0337efc02e094c4be930
bok <102132293+bok000111@users.noreply.github.com>
2023-11-06 12:57:43 +0000

    [feat] impl custom antd theme

---
+++ styles/theme/ButtonTheme.ts
@@ -0,0 +1,41 @@
+const colorPrimary0 = "#132743";
+const colorPrimary1 = "#4a6282";
+const colorPrimary2 = "#88a0c3";
+const colorSecondary0 = "#ffb5b5";
+const colorSecondary1 = "#ffc6c6";
+const colorSecondary2 = "#ffdddd";
+const colorGrey0 = "#3f4135";
+const colorGrey1 = "#7f848d";
+const colorGrey2 = "#d9d9d9";
+const colorGreywhite0 = "#fcfdff";
+const colorRed0 = "#b20000";
+const colorGreen0 = "#00621b";
+
+const ButtonTheme = {
+  // color
+  defaultColor: colorPrimary0,
+  defaultBorderColor: colorPrimary0,
+
+  colorPrimary: colorPrimary0,
+  colorPrimaryActive: colorPrimary0,
+  colorPrimaryText: colorGreywhite0,
+
+  colorError: colorRed0,
+  colorErrorBg: colorGreywhite0,
+  // style
+  size: 32,
+  borderRadiusSM: 24,
+  borderRadius: 32,
+  borderRadiusLG: 40,
+  onlyIconSizeSM: 16,
+  onlyIconSize: 24,
+  onlyIconSizeLG: 32,
+
+  // font
+  contentFontSizeSM: 16,
+  contentFontSize: 20,
+  contentFontSizeLG: 24,
+  fontWeight: 500,
+};
+
+export default ButtonTheme;
