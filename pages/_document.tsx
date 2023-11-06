7b39c5d6161b96cb15e8d1083254db572494d774
bok <102132293+bok000111@users.noreply.github.com>
2023-11-06 12:55:27 +0000

    [feat] add default antd setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,56 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import type { DocumentContext } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <style></style>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+const MyDocument = () => (
+  <Html lang="en">
+    <Head>
+      <link
+        rel="preload"
+        href="~&/fonts/woff2/GmarketSansMedium.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="~&/fonts/woff/GmarketSansMedium.woff"
+        as="font"
+        type="font/woff"
+        crossOrigin=""
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => (
+        <StyleProvider cache={cache}>
+          <App {...props} />
+        </StyleProvider>
+      ),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const style = extractStyle(cache, true);
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    ),
+  };
+};
+
+export default MyDocument;
