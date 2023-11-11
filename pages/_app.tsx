7a6e52946d4969af8a290e8666ff9329473ceffe
Mike <mg5566@hanmail.net>
2023-11-11 05:14:32 +0000

    [feat] create redirection logic
    로그인이 되지 않았다면 redirection 시키는 logic 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,22 @@ import Modal from "@/atoms/Modal/Modal";
 
 import theme from "%/theme/themeConfig";
 
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }: AppProps) {
+  // 로그인을 하지 않았다면
+  const isAuth = false;
+  const router = useRouter();
+
+  useEffect(() => {
+    const protectedRoutes = ["/main"];
+
+    if (!isAuth && protectedRoutes.includes(router.pathname)) {
+      router.push("/");
+    }
+  }, [isAuth, router]);
+
   return (
     <ConfigProvider theme={theme}>
       <Modal />
