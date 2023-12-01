6ed3bf8e928a8e787ef39eb6525ceea3389db16e
bok000111 <bok000111@naver.com>
2023-12-01 03:25:32 +0000

    [feat] refactoring UserTable

--- src/molecules/UserTable/UserTable.tsx
+++ src/molecules/UserTable/UserTable.tsx
@@ -1,39 +1,36 @@
 import React from "react";
-import FunctionButtonIcon from "&/Icons/functionButton.svg";
-import UserAddIcon from "&/Icons/userAdd.svg";
 import User from "@/types/User";
 import { Size } from "@/types/enums";
-import Checkbox from "@/atoms/buttons/Checkbox/Checkbox";
-import IconButton from "@/atoms/buttons/IconButton/IconButton";
 import ProfileCard from "../ProfileCard/ProfileCard";
 
 import styles from "./UserTable.module.scss";
+import Group from "@/types/Group";
 
 type UserTableProps = {
-  users?: (User & { isChecked?: boolean })[];
-  isInEditMode?: boolean;
+  users?: (User & {
+    Icon?: React.ReactNode;
+    onClick?: React.MouseEventHandler;
+  })[];
+  parentGroup: Group;
   size: Size;
 };
 
-const UserTable: React.FC<UserTableProps> = ({
-  users,
-  isInEditMode = false,
-  size,
-}) => {
-  const ProfileComponentList = users?.map((user, i) => (
-    <ProfileCard key={i} user={user} size={size}>
-      {isInEditMode ? (
-        <Checkbox isChecked={user?.isChecked} size={size} />
-      ) : user?.isFriend ? (
-        <IconButton Icon={FunctionButtonIcon} size={size} />
-      ) : (
-        <IconButton Icon={UserAddIcon} size={size} />
-      )}
-    </ProfileCard>
-    // TODO: 이거 나중에 리팩토링 어떻게하지?????
-  ));
-
-  return <div className={styles["profile-table"]}>{ProfileComponentList}</div>;
+const UserTable: React.FC<UserTableProps> = ({ users, size, parentGroup }) => {
+  return (
+    <div className={styles["profile-table"]}>
+      {users?.map((user) => (
+        <ProfileCard
+          key={user.id}
+          user={user}
+          size={size}
+          onClick={user.onClick}
+          parentGroup={parentGroup}
+        >
+          {user.Icon}
+        </ProfileCard>
+      ))}
+    </div>
+  );
 };
 
 export default UserTable;
