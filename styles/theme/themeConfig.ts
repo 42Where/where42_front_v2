a3c80e8cf7ff99a40547659dd27804e14ff22f8f
bok000111 <bok000111@naver.com>
2023-12-02 15:49:41 +0000

    [feat] refactoring theme
    분리했던 파일 합치고 기본 색상 추가

--- styles/theme/themeConfig.ts
+++ styles/theme/themeConfig.ts
@@ -2,7 +2,6 @@
 import type { ThemeConfig } from "antd";
 
 import color from "./Colors";
-import ButtonTheme from "./ButtonTheme";
 
 const theme: ThemeConfig = {
   token: {
@@ -12,9 +11,64 @@ const theme: ThemeConfig = {
 
     colorPrimary: color.Primary0,
     colorError: color.Red0,
+    colorSuccess: color.Green0,
+    colorIcon: color.Primary0,
+    colorBgLayout: color.Greywhite0,
+    colorTextBase: color.Primary0,
   },
   components: {
-    Button: ButtonTheme,
+    Button: {
+      algorithm: true,
+      // color
+      primaryColor: color.Greywhite0,
+
+      defaultColor: color.Primary0,
+      defaultBg: color.Greywhite0,
+      defaultBorderColor: color.Primary0,
+
+      dangerColor: color.Greywhite0,
+      borderColorDisabled: color.Grey2,
+
+      colorPrimary: color.Primary0,
+      colorPrimaryHover: color.Primary1,
+      colorPrimaryActive: color.Primary0,
+      colorPrimaryText: color.Greywhite0,
+
+      colorError: color.Red0,
+      colorErrorBg: color.Greywhite0,
+      // style
+      size: 32,
+      borderRadiusSM: 24,
+      borderRadius: 32,
+      borderRadiusLG: 40,
+      onlyIconSizeSM: 16,
+      onlyIconSize: 24,
+      onlyIconSizeLG: 32,
+
+      // font
+      contentFontSizeSM: 16,
+      contentFontSize: 20,
+      contentFontSizeLG: 24,
+      fontWeight: 500,
+    },
+    Menu: {
+      algorithm: true,
+
+      controlItemBgActive: color.Primary2,
+      colorBgTextHover: color.Grey2,
+    },
+    Modal: {
+      algorithm: true,
+
+      // TODO: 이거 버튼 색이 이상함 어케함???????????
+      fontWeightStrong: 500,
+    },
+    Cascader: {
+      algorithm: true,
+
+      controlItemBgActive: color.Primary2,
+      colorBgTextHover: color.Grey2,
+    },
   },
 };
 
