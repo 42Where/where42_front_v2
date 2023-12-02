681e992c4bd06a6bbef2b72cbbcdad4f771ab17e
bok000111 <bok000111@naver.com>
2023-12-02 15:50:05 +0000

    [feat] impl useGroupSelectModal

---
+++ src/hooks/useGroupSelectModal.tsx
@@ -0,0 +1,176 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { Modal, ModalProps, Menu, Empty } from "antd";
+import { MenuInfo } from "rc-menu/lib/interface";
+
+import User from "@/types/User";
+import Group from "@/types/Group";
+
+type GroupSelectModalProps<T, U> = {
+  onOk: ((value: number[]) => void) | ((value: number[]) => Promise<T>);
+  onCancel?: (() => void) | (() => Promise<U>);
+  icon?: React.ReactNode;
+  title: React.ReactNode;
+  grouplist: Group[];
+  targetUser: User;
+  component?: React.ReactNode;
+  danger?: boolean;
+  maskClosable?: boolean;
+  okText?: string;
+  cancelText?: string;
+};
+
+const useGroupSelectModal = <T, U, V>({
+  onOk,
+  onCancel,
+  icon,
+  title,
+  grouplist,
+  targetUser,
+  component,
+  danger = false,
+  maskClosable = true,
+  okText,
+  cancelText,
+}: GroupSelectModalProps<T, U>) => {
+  const [visible, setVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  // const [selectedGroups, setSelectedGroups] = useState<Group[]>([]);
+  const [selectedGroupIds, setSelectedGroupIds] = useState<number[]>([]);
+
+  useEffect(() => {
+    // setSelectedGroups([]);
+    setSelectedGroupIds([]);
+  }, []);
+
+  const handleSelect = useCallback(
+    (menuInfo: MenuInfo) => {
+      menuInfo.domEvent.preventDefault();
+      const selectedGroupId = parseInt(menuInfo.key as string);
+      setSelectedGroupIds([...selectedGroupIds, selectedGroupId]);
+    },
+    [setSelectedGroupIds, selectedGroupIds]
+  );
+
+  const handleDeselect = useCallback(
+    (menuInfo: MenuInfo) => {
+      menuInfo.domEvent.preventDefault();
+      const selectedGroupId = parseInt(menuInfo.key as string);
+      setSelectedGroupIds(
+        selectedGroupIds.filter((id) => id !== selectedGroupId)
+      );
+    },
+    [setSelectedGroupIds, selectedGroupIds]
+  );
+
+  const handleOk = useCallback(
+    async (e: React.MouseEvent) => {
+      e.preventDefault();
+      if (loading) {
+        return; // 이미 처리 중인 경우 중복 호출 방지
+      }
+
+      setLoading(true);
+      try {
+        const result = onOk(selectedGroupIds);
+        if (result instanceof Promise) {
+          await result;
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+        setVisible(false);
+      }
+    },
+    [onOk, loading, setLoading, setVisible, selectedGroupIds]
+  );
+
+  const handleCancel = useCallback(
+    async (e: React.MouseEvent) => {
+      e.preventDefault();
+      if (loading) {
+        return; // 이미 처리 중인 경우 중복 호출 방지
+      }
+      setLoading(true);
+      try {
+        if (onCancel) {
+          const result = onCancel();
+          if (result instanceof Promise) {
+            await result;
+          }
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setSelectedGroupIds([]);
+        setLoading(false);
+        setVisible(false);
+      }
+    },
+    [onCancel, loading, setLoading, setVisible, setSelectedGroupIds]
+  );
+
+  const showModal = useCallback(() => {
+    setVisible(true);
+  }, [setVisible]);
+  const hideModal = useCallback(handleCancel, [handleCancel]);
+
+  const modalProps: ModalProps = {
+    open: visible,
+    title: (
+      <>
+        {icon}
+        {title}
+      </>
+    ),
+    onOk: handleOk,
+    onCancel: handleCancel,
+    confirmLoading: loading,
+    maskClosable: maskClosable,
+    okText: okText,
+    cancelText: cancelText,
+    destroyOnClose: true,
+    okButtonProps: {
+      danger: danger,
+      disabled: loading || selectedGroupIds.length === 0,
+    },
+    cancelButtonProps: { disabled: loading },
+  };
+  return {
+    modal: (
+      <Modal {...modalProps}>
+        <>
+          {grouplist.length === 0 ? (
+            // TODO: 사용자 아이디는 볼드처리 필요
+            // TODO: 추가할 그룹이 없을때 Empty를 띄울지 모달을 띄우지않고 메시지를 띄울지 결정 필요
+            <>{`${targetUser.login}님은 이미 모든 그룹에 포함되어있습니다`}</>
+          ) : (
+            <>
+              {component}
+              <Menu
+                multiple={true}
+                onSelect={handleSelect}
+                onDeselect={handleDeselect}
+                selectedKeys={selectedGroupIds.map((id) => id.toString())}
+              >
+                {grouplist.map((group) => {
+                  return (
+                    <Menu.Item key={group.id}>
+                      {group.name}{" "}
+                      {group.users.filter((user) => user.location).length}/
+                      {group.users.length}
+                    </Menu.Item>
+                  );
+                })}
+              </Menu>
+            </>
+          )}
+        </>
+      </Modal>
+    ),
+    showModal,
+    hideModal,
+  };
+};
+
+export default useGroupSelectModal;
