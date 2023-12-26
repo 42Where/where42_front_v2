06b996e6d129c00eed0258615429794f8141eb34
bok000111 <bok000111@naver.com>
2023-12-26 22:04:13 +0000

    [feat] refactoring user store

---
+++ src/stores/useUserStore.ts
@@ -0,0 +1,53 @@
+import { create } from "zustand";
+
+import User from "@/types/User";
+
+export type UserStore = {
+  user?: Pick<
+    User,
+    | "id"
+    | "login"
+    | "profileImgSrc"
+    | "location"
+    | "comment"
+    | "locationUsageAgreement"
+  >;
+  attendanceOnly: boolean;
+  token?: string;
+  refreshToken?: string;
+  /**
+   * 일단 넣어두긴 했는데 당장은 사용하지 않음
+   */
+  darkMode?: boolean;
+  setUser: (user: User) => void;
+  setLocation: (location: string) => void;
+  setComment: (comment: string) => void;
+  setToken: (token: string) => void;
+  setAttendanceOnly: (attendanceOnly: boolean) => void;
+};
+
+const useUserStore = create<UserStore>((set) => ({
+  attendanceOnly: false,
+  setUser: (user) => set((state) => ({ ...state, user: user })),
+  setLocation: (location) =>
+    set((state) => ({
+      ...state,
+      user: state.user ? { ...state.user, location } : state.user,
+    })),
+  setComment: (comment) => {
+    set((state) => ({
+      ...state,
+      user: state.user ? { ...state.user, comment } : state.user,
+    }));
+  },
+  setToken: (token) => {
+    set((state) => ({
+      ...state,
+      token,
+    }));
+  },
+  setAttendanceOnly: (attendanceOnly) =>
+    set((state) => ({ ...state, attendanceOnly })),
+}));
+
+export default useUserStore;
