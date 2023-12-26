deb29c204cf98624a03c5d2a4de504970591cbff
bok000111 <bok000111@naver.com>
2023-12-26 22:05:11 +0000

    [feat] impl responsive design for profile text
    refactoring css with tailwind css

--- src/molecules/ProfileCard/ProfileText.tsx
+++ src/molecules/ProfileCard/ProfileText.tsx
@@ -1,60 +1,32 @@
 import React from "react";
 
 import User from "@/types/User";
-import { Size } from "@/types/enums";
 import TextBox from "@/atoms/TextBox/TextBox";
 
-import styles from "./ProfileText.module.scss";
+import styles from "./ProfileText.module.css";
 
 type ProfileTextProps = {
   /**
    * 표시할 텍스트 정보가 들어있는 User 객체입니다.
    */
   user: User;
-  /**
-   * 컴포넌트의 크기입니다.
-   */
-  size: Size;
 };
 
-const ProfileText: React.FC<ProfileTextProps> = ({ user, size }) => {
-  const profileTextStyle =
-    styles["profile-text"] + " " + styles["profile-text--" + size];
-  const mainTextStyle = styles["profile-text__main-text"];
-  const loginStyle =
-    styles["profile-text__login"] +
-    " " +
-    styles["profile-text__login--" + size];
-  const commentStyle =
-    styles["profile-text__comment"] +
-    " " +
-    styles["profile-text__comment--" + size];
-
+const ProfileText: React.FC<ProfileTextProps> = ({ user }) => {
   const locationText = user.location ? user.location : "퇴근";
   // TODO: 백엔드 api 구현 후에 로직 변경 필요할수도 있음
 
   return (
-    <div className={profileTextStyle}>
-      <div className={mainTextStyle}>
-        <div className={loginStyle}>{user.login}</div>
-        <TextBox
-          text={locationText}
-          primary={user.location !== undefined}
-          size={size}
-        />
+    <div className={styles.profile_text}>
+      <div className={styles.main}>
+        <div className={styles.login}>{user.login}</div>
+        <TextBox text={locationText} primary={user.location !== undefined} />
       </div>
       {user.comment === "" ? null : (
-        <div className={commentStyle}>{user.comment}</div>
+        <div className={styles.comment}>{user.comment}</div>
       )}
     </div>
   );
 };
 
-export default React.memo(ProfileText, (prevProps, nextProps) => {
-  return (
-    prevProps.user.login === nextProps.user.login &&
-    prevProps.user.location === nextProps.user.location &&
-    prevProps.user.comment === nextProps.user.comment &&
-    prevProps.size === nextProps.size
-  );
-});
+export default ProfileText;
