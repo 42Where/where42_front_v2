390837d6257e956c65e572f9dc53829bbf6ee517
bok000111 <bok000111@naver.com>
2023-12-26 22:07:43 +0000

    [feat] refactoring float button

--- src/molecules/AFloatButton/AFloatButton.tsx
+++ src/molecules/AFloatButton/AFloatButton.tsx
@@ -1,38 +1,22 @@
 import React from "react";
-
 import { Button, ButtonProps } from "antd";
+
+import styles from "./AFloatButton.module.css";
 
 type AFloatButtonProps = {
   isVisible: boolean;
   buttonProps: ButtonProps[];
 };
 
-const AFloatButton = ({ buttonProps, isVisible }: AFloatButtonProps) => {
+const AFloatButton: React.FC<AFloatButtonProps> = ({
+  buttonProps,
+  isVisible,
+}) => {
   const buttonWrapper = isVisible ? (
-    <div
-      style={{
-        position: "fixed",
-        bottom: "1rem",
-        right: "1rem",
-        display: "flex",
-        alignItems: "flex-end",
-        gap: "10px",
-        padding: "10px",
-        zIndex: 1000,
-      }}
-    >
-      {buttonProps.map((props) => {
-        return (
-          <Button
-            key={props.name}
-            {...props}
-            style={{
-              boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
-              backgroundColor: "white",
-            }}
-          />
-        );
-      })}
+    <div className={styles.wrapper}>
+      {buttonProps.map((props) => (
+        <Button key={props.name} {...props} className="shadow-md bg-white" />
+      ))}
     </div>
   ) : null;
 
