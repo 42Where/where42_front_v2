2dfa3676ce452bce180fc6b803c5be90eaa97ad8
bok000111 <bok000111@naver.com>
2023-12-26 22:07:11 +0000

    [feat] refactoring header

--- src/molecules/Header/Header.tsx
+++ src/molecules/Header/Header.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { useMediaQuery } from "react-responsive";
 
-import Logo from "&/Icons/logoB.svg";
+import Logo from "&/Icons/logoC.svg";
 import SearchIcon from "&/Icons/search.svg";
 import SignOutIcon from "&/Icons/signOut.svg";
 
 import AIcon from "@/atoms/AIcon/AIcon";
-
-import styles from "./Header.module.scss";
 import SearchModal from "../SearchModal/SearchModal";
+
+import styles from "./Header.module.css";
+import { useIsLaptop, useIsMobile, useIsTablet } from "@/utils/MediaQuary";
 
 const Header: React.FC = () => {
   const [currentPath, setCurrentPath] = useState("");
@@ -19,43 +21,40 @@ const Header: React.FC = () => {
     }
   }, []);
   const router = useRouter();
+  const isMobile = useIsMobile();
+  const isTablet = useIsTablet();
+  const isLaptop = useIsLaptop();
 
-  const LogoClickHandler = () => {
+  const LogoClickHandler = useCallback(() => {
     router.push("/main");
-    // TODO: 메인페이지로 이동
-    // 구현 변경이 필요할수도 있음
-  };
-  const SearchClickHandler = () => {
+  }, [router]);
+  const SearchClickHandler = useCallback(() => {
     setSearchModalOpen(true);
-    // TODO: 검색창으로 이동
-    // 추후 검색창으로 이동이 아니라 플로트를 띄우는 방식으로 변경
-  };
+  }, []);
   const SignOutClickHandler = () => {
     // TODO: 대충 로그아웃 함수 구현 필요
     // 재활용 가능성이 많은 함수이므로 다른곳에서 구현후 import해서 재사용해야할수도 있음
   };
 
-  const LogoComponent = <Logo onClick={LogoClickHandler} />;
+  const LogoComponent = (
+    <Logo
+      width={isMobile || isTablet ? "6rem" : "8rem"}
+      onClick={LogoClickHandler}
+    />
+  );
   const Buttons =
     currentPath === "/main" ? (
       <>
         <AIcon
           icon={SearchIcon}
           onClick={SearchClickHandler}
-          size="medium"
+          size={isMobile || isTablet ? "medium" : "large"}
           key={"search"}
-        />
-        <SearchModal
-          open={searchModalOpen}
-          size="medium"
-          onCancel={() => {
-            setSearchModalOpen(false);
-          }}
         />
         <AIcon
           icon={SignOutIcon}
           onClick={SignOutClickHandler}
-          size="medium"
+          size={isMobile || isTablet ? "medium" : "large"}
           key={"signout"}
         />
       </>
@@ -64,12 +63,16 @@ const Header: React.FC = () => {
   return (
     <div className={styles.header}>
       {LogoComponent}
-      <div className={styles["header__buttonwrapper"]}>{Buttons}</div>
+      <div className={styles.buttonwrapper}>{Buttons}</div>
+      <SearchModal
+        open={searchModalOpen}
+        size={isMobile || isTablet ? "medium" : "large"}
+        onCancel={() => {
+          setSearchModalOpen(false);
+        }}
+      />
     </div>
   );
-
-  // TODO?: 추후에 이미지 사이즈를 그대로 사용하는 아이콘 버튼이 필요할경우
-  // IconButton의 props를 수정하거나 별개의 컴포넌트로 구현이 필요할수도 있음*
 };
 
 export default Header;
