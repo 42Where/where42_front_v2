480511f46cbf1fea55b2e1bed77972a8d285494a
OKESTRO <mike@OKESTROui-MacBookPro-2.local>
2024-02-17 18:19:12 +0900

    [test] set userList to dummy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,52 +20,52 @@ export default function App({ Component, pageProps }: AppProps) {
   const path = router.pathname;
   const replace = router.replace;
 
-  useEffect(() => {
-    const handleRedirect = () => {
-      switch (path) {
-        case "/main":
-          if (!token) replace("/login");
-          break;
-        case "/login":
-          if (token) {
-            replace("/main");
-          }
-          break;
-        default:
-          if (token) {
-            replace("/main");
-          } else {
-            replace("/login");
-          }
-          break;
-      }
-    };
+  // useEffect(() => {
+  //   const handleRedirect = () => {
+  //     switch (path) {
+  //       case "/main":
+  //         if (!token) replace("/login");
+  //         break;
+  //       case "/login":
+  //         if (token) {
+  //           replace("/main");
+  //         }
+  //         break;
+  //       default:
+  //         if (token) {
+  //           replace("/main");
+  //         } else {
+  //           replace("/login");
+  //         }
+  //         break;
+  //     }
+  //   };
 
-    handleRedirect();
-  }, [path, replace, token]);
+  //   handleRedirect();
+  // }, [path, replace, token]);
 
-  useEffect(() => {
-    // 쿼리스트링에서 토큰 가져오기
-    console.log(router.query);
-    const { token, intraId, agreement } = router.query;
+  // useEffect(() => {
+  //   // 쿼리스트링에서 토큰 가져오기
+  //   console.log(router.query);
+  //   const { token, intraId, agreement } = router.query;
 
-    if (intraId && agreement && token) {
-      // 토큰이 존재하면 로컬 스토리지에 저장
-      localStorage.setItem("token", token as string);
-      console.log("Token update from query string:", token);
-      setToken(token as string);
-      memberApi
-        .getMemberInfo({ intraId: Number(intraId as string) })
-        .then((res) => {
-          console.log(res);
-          setUser(res);
-        });
-      return;
-    } else if (token) {
-      const storedToken = localStorage.getItem("token");
-      if (storedToken) setToken(storedToken);
-    }
-  }, [router.query, setToken, setUser]);
+  //   if (intraId && agreement && token) {
+  //     // 토큰이 존재하면 로컬 스토리지에 저장
+  //     localStorage.setItem("token", token as string);
+  //     console.log("Token update from query string:", token);
+  //     setToken(token as string);
+  //     memberApi
+  //       .getMemberInfo({ intraId: Number(intraId as string) })
+  //       .then((res) => {
+  //         console.log(res);
+  //         setUser(res);
+  //       });
+  //     return;
+  //   } else if (token) {
+  //     const storedToken = localStorage.getItem("token");
+  //     if (storedToken) setToken(storedToken);
+  //   }
+  // }, [router.query, setToken, setUser]);
 
   // useEffect(() => {
   //   // 토큰이 존재하면 axios 헤더에 토큰 추가, 없으면 삭제
--- src/molecules/SearchModal/SearchModal.tsx
+++ src/molecules/SearchModal/SearchModal.tsx
@@ -6,6 +6,7 @@ import User from "@/types/User";
 import { Size } from "@/types/enums";
 import UserTable from "../UserTable/UserTable";
 import { useSize } from "@/utils/MediaQuary";
+import memberApi from "@/api/memberApi";
 // import useMyDataStore from "@/stores/useMyDataStore";
 
 // import styles from "./SearchModal.module.scss";
@@ -27,19 +28,47 @@ const SearchModal: React.FC<SearchModalProps> = ({ open, size, onCancel }) => {
   //   myData: { id },
   // } = useMyDataStore((state) => state);
 
-  const onSearch: SearchProps["onSearch"] = (v, e) => {
-    e?.preventDefault();
-    // Axios.get("/v3/search", { params: { intraId: id, keyWord: v } })
+  const onSearch: SearchProps["onSearch"] = (value, _e, info) => {
+    _e?.preventDefault();
+
+    console.log(info?.source, value);
+
+    // create dummy data
+    const dummyUserList: User[] = [
+      {
+        intraId: 0,
+        intraName: "김민수",
+        image: "https://cdn.intra.42.fr/users/minsu.jpg",
+        inCluster: true,
+        agree: true,
+      },
+      {
+        intraId: 1,
+        intraName: "박민수",
+        image: "https://cdn.intra.42.fr/users/minsu.jpg",
+        inCluster: true,
+        agree: true,
+      },
+      {
+        intraId: 2,
+        intraName: "이민수",
+        image: "https://cdn.intra.42.fr/users/minsu.jpg",
+        inCluster: true,
+        agree: true,
+      },
+    ];
+    setUserList(dummyUserList);
+
+    // memberApi
+    //   .getMemberInfo({ intraId: Number(value) })
     //   .then((res) => {
-    //     // setUserList(res.data);
-    //     console.log(res.data);
+    //     console.log(res);
     //   })
     //   .catch((err) => {
     //     console.log(err);
     //   });
-    // TODO: 임시 구현
-    // api 호출 후에 setUserList로 상태 업데이트 구현 필요
   };
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setInputValue(e.target.value);
