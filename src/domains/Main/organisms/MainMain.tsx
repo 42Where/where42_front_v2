2a4091f37aa1aa7f9ad12054239c9bdffaf0ca85
jbok <jbok@student.32seoul.kr>
2024-02-24 01:50:54 +0000

    [fix] Remove unused code and API call in MainMain component

--- src/domains/Main/organisms/MainMain.tsx
+++ src/domains/Main/organisms/MainMain.tsx
@@ -15,23 +15,8 @@ import styles from "./MainMain.module.css";
 import { Skeleton } from "antd";
 
 function MainMain() {
-  const { user, setUser, token } = useMyDataStore((state) => state);
+  const { user, setUser } = useMyDataStore((state) => state);
   const { groups, setGroups } = useGroupStore((state) => state);
-  useEffect(() => {
-    if (token) {
-      groupApi
-        .getAllGroups()
-        .then((res) => {
-          setGroups(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    // 테스트용 더미유저와 그룹을 넣어줌
-    // api 호출하는것처럼 보이게 하기 위해 비동기 demoApi 함수 사용
-    // TODO: 인증 완료되면 api 사용해서 갱신해놓고 루트페이지에서 바로 메인페이지로 이동
-  }, [setGroups, token]);
   return (
     <div className={styles.main_content}>
       <UserProfileCard />
