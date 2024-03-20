e4a06b61e48722792491e168d44a5eccfbafa4f2
daejlee <savif4991@gmail.com>
2024-03-20 15:54:39 +0900

    Test: Disable temoprarily

--- src/lib/Axios.ts
+++ src/lib/Axios.ts
@@ -25,49 +25,49 @@ axios.interceptors.response.use(
   async (response) => {
     console.log(response.config.url, response.config.data, response.status);
     return response;
-  },
-  async (error) => {
-    console.log('에러 났음');
-    console.log(error.response);
-    console.log(error.response.status);
-    console.log('type: ', typeof error.response.status);
-    console.log('type: ', typeof 401);
-    console.log('res: ', error.response.status == 401);
-    // 야 여기서 라우터 쓰면 안되나봐
-    if (
-      error.response &&
-      error.response.status == 401 &&
-      !error.config.__isRetryRequest
-    ) {
-      console.log('여기도 왔음');
-      const prevAccessToken = Cookies.get('accessToken');
-      const refreshToken = Cookies.get('refreshToken');
-      if (refreshToken) {
-        error.config.__isRetryRequest = true;
-        console.log('심지어 여기도 왔음');
-        try {
-          console.log('세상에나 여기도 왔음');
-          const res = await authApi.reissueToken(refreshToken);
-          console.log('결과는?: ', res);
-          // Cookies.set('accessToken', res.refreshToken);
-          console.log('Refreshed token successfully!');
-        } catch (err) {
-          // const accessToken = res.data.accessToken;
-          // const originalRequest = error.config;
-          // originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-          // return await axios(originalRequest);
-          console.error('Failed to refresh token:', err);
-        }
-      } else {
-        console.error('야 이거 왜 안되는거야');
-        console.log('No refresh token');
-        Cookies.remove('accessToken');
-        Cookies.remove('refreshToken');
-        // router.push('/login');
-      }
-    }
-    return Promise.reject(error);
   }
+  // async (error) => {
+  //   console.log('에러 났음');
+  //   console.log(error.response);
+  //   console.log(error.response.status);
+  //   console.log('type: ', typeof error.response.status);
+  //   console.log('type: ', typeof 401);
+  //   console.log('res: ', error.response.status == 401);
+  //   // 야 여기서 라우터 쓰면 안되나봐
+  //   if (
+  //     error.response &&
+  //     error.response.status == 401 &&
+  //     !error.config.__isRetryRequest
+  //   ) {
+  //     console.log('여기도 왔음');
+  //     const prevAccessToken = Cookies.get('accessToken');
+  //     const refreshToken = Cookies.get('refreshToken');
+  //     if (refreshToken) {
+  //       error.config.__isRetryRequest = true;
+  //       console.log('심지어 여기도 왔음');
+  //       try {
+  //         console.log('세상에나 여기도 왔음');
+  //         const res = await authApi.reissueToken(refreshToken);
+  //         console.log('결과는?: ', res);
+  //         // Cookies.set('accessToken', res.refreshToken);
+  //         console.log('Refreshed token successfully!');
+  //       } catch (err) {
+  //         // const accessToken = res.data.accessToken;
+  //         // const originalRequest = error.config;
+  //         // originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+  //         // return await axios(originalRequest);
+  //         console.error('Failed to refresh token:', err);
+  //       }
+  //     } else {
+  //       console.error('야 이거 왜 안되는거야');
+  //       console.log('No refresh token');
+  //       Cookies.remove('accessToken');
+  //       Cookies.remove('refreshToken');
+  //       // router.push('/login');
+  //     }
+  //   }
+  //   return Promise.reject(error);
+  // }
 );
 
 export default axios;
