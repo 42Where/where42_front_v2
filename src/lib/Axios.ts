351cc8d5665cec235c81fd8543ed60d78e89c502
daejlee <savif4991@gmail.com>
2024-03-20 22:23:06 +0900

    Feat: Add data re-fetch

--- src/lib/Axios.ts
+++ src/lib/Axios.ts
@@ -27,34 +27,30 @@ axios.interceptors.response.use(
     return response;
   },
   async (error) => {
-    console.log('에러 났음');
     console.log(error.response);
     console.log(error.response.status);
-    // 야 여기서 라우터 쓰면 안되나봐
     if (error.response && error.response.status == 401) {
-      console.log('여기도 왔음');
-      const prevAccessToken = Cookies.get('accessToken');
       const refreshToken = Cookies.get('refreshToken');
       if (refreshToken) {
-        console.log('심지어 여기도 왔음');
         try {
-          console.log('세상에나 여기도 왔음');
           const res = await authApi.reissueToken(refreshToken);
-          console.log('결과는?: ', res);
-          // Cookies.set('accessToken', res.refreshToken);
+          Cookies.set('accessToken', res.accessToken);
           console.log('Refreshed token successfully!');
-          // const accessToken = res.data.accessToken;
-          // const originalRequest = error.config;
-          // originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-          // return await axios(originalRequest);
+          const accessToken = res.accessToken;
+          const originalRequest = error.config;
+          originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+          return await axios(originalRequest);
         } catch (err) {
           console.error('Failed to refresh token:', err);
+          Cookies.remove('accessToken');
+          Cookies.remove('refreshToken');
+          window.location.href = '/login';
         }
       } else {
-        console.error('야 이거 왜 안되는거야');
         console.log('No refresh token');
         Cookies.remove('accessToken');
         Cookies.remove('refreshToken');
+        window.location.href = '/login';
       }
     }
     return Promise.reject(error);
