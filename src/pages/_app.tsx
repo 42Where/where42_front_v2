a8c1b37f2a2914ae7e60ea689aa10f8c3fba5e14
daejlee <savif4991@gmail.com>
2024-03-20 12:57:32 +0900

    Feat: Add title and fabicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Toaster } from '@/components/ui/toaster';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Toaster />
+      <Head>
+        <title>Where42</title>
+        <link rel='icon' href='/icons/logo.svg' />
+      </Head>
       <Component {...pageProps} />
     </>
   );
