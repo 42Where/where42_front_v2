ca8383569cf48f2abea041f9ea7e4068511e6e8d
daejlee <savif4991@gmail.com>
2024-05-13 12:44:45 +0900

    Style: Fix text

--- src/pages/copyright.txt/index.tsx
+++ src/pages/copyright.txt/index.tsx
@@ -2,12 +2,12 @@ export default function CopyrightTxt() {
   return (
     <div>
       <p>
-        앱 &apos;키즈스쿨 (kr.kidsschool)&apos;의 소유자와 이 웹사이트의
-        소유주가 동일하다는 것을 증명합니다.
+        앱 &apos;where42 (https://test.where42.kr/)&apos;의 소유자와 이
+        웹사이트의 소유주가 동일하다는 것을 증명합니다.
       </p>
       <p>
         This text evidences that same person(owner) has copyright about
-        app(kr.kidsschool) and this site.
+        app(https://test.where42.kr/) and this site.
       </p>
     </div>
   );
