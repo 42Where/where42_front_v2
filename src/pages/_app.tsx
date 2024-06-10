a004a462d6c27ee90ee3ddc1da96db90a18371fa
daejlee <savif4991@gmail.com>
2024-06-10 21:43:47 +0900

    Feat: Add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Toaster } from '@/components/ui/toaster';
 import Head from 'next/head';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel='icon' href='/Icons/logo.svg' />
       </Head>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId='G-0H3Z5S8GSR' />
     </>
   );
 }
