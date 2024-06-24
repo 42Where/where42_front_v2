2f36954814f179557ee7f970da508f52f5022728
daejlee <savif4991@gmail.com>
2024-06-24 20:37:52 +0900

    Style: Change copyright

--- src/pages/copyright.txt/index.tsx
+++ src/pages/copyright.txt/index.tsx
@@ -2,12 +2,12 @@ export default function CopyrightTxt() {
   return (
     <div>
       <p>
-        앱 &apos;where42 (https://test.where42.kr/)&apos;의 소유자와 이
-        웹사이트의 소유주가 동일하다는 것을 증명합니다.
+        앱 &apos;where42 (https://where42.kr/)&apos;의 소유자와 이 웹사이트의
+        소유주가 동일하다는 것을 증명합니다.
       </p>
       <p>
         This text evidences that same person(owner) has copyright about
-        app(https://test.where42.kr/) and this site.
+        app(https://where42.kr/) and this site.
       </p>
     </div>
   );
