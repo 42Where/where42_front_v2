19ebbde1ce503fcc7161a0068b6d18007790c83a
daejlee <savif4991@gmail.com>
2024-07-03 00:44:55 +0900

    Feat: Change href

--- src/components/Buttons/LoginBtn.tsx
+++ src/components/Buttons/LoginBtn.tsx
@@ -7,7 +7,7 @@ export default function LoginBtn({ isMobile }: { isMobile?: boolean }) {
 
   return !isClicked ? (
     <Link
-      href='/v3/group/'
+      href={`${process.env.NEXT_PUBLIC_DEV_API_URL}/v3/group/`}
       className={`${isMobile ? 'absolute z-auto' : 'hidden sm:block'}`}
       onClick={() => setIsClicked(true)}
     >
