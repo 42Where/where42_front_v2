4555462faa9edcf571f54f7a3e7b745af9f8d076
daejlee <savif4991@gmail.com>
2024-09-06 03:52:12 +0900

    Feat: remove asChild, set base tooltip direction

--- src/components/Modals/GroupSettingModal.tsx
+++ src/components/Modals/GroupSettingModal.tsx
@@ -48,7 +48,7 @@ export default function GroupSettingModal({ curGroup }: { curGroup: Group }) {
       }}
     >
       <DropdownMenu>
-        <DropdownMenuTrigger asChild>
+        <DropdownMenuTrigger>
           <GroupSettingBtn groups={groups} curGroup={curGroup} />
         </DropdownMenuTrigger>
         <DropdownMenuContent
