419a6e4c1b842614011916238e1f52fdb08e068c
daejlee <savif4991@gmail.com>
2024-09-24 15:47:18 +0900

    Feat: Repo link component added

---
+++ src/components/utils/RepoLink.tsx
@@ -0,0 +1,32 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function RepoLink() {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link href="https://github.com/42Where">
+            <Image
+              src="image/logo/logoC.svg"
+              alt="logo"
+              width={120}
+              height={120}
+              className="
+        w-[80px] md:w-[120px]"
+            />
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p className="text-l lg:text-xl">깃허브 레포지토리 방문</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
