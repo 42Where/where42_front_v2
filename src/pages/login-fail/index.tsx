ef905d52f772bc2a0d7b40217bd552c43a458cab
daejlee <savif4991@gmail.com>
2024-09-24 14:57:38 +0900

    Chore: login-fail page css fixed

--- src/pages/login-fail/index.tsx
+++ src/pages/login-fail/index.tsx
@@ -14,14 +14,6 @@ export default function LoginFailPage() {
         />
       </div>
       <div className="w-full bg-white sm:w-1/2">
-        {/* <Image
-          src='/Icons/help.svg'
-          alt='help'
-          width={80}
-          height={80}
-          priority
-          className='absolute top-2 right-2 size-10 sm:top-4 sm:right-4 sm:size-16'
-        /> */}
         <div className="flex h-full min-h-[50vh] flex-col items-center justify-center gap-8 sm:gap-8">
           <Image
             src="/image/logo/logo.svg"
@@ -32,12 +24,12 @@ export default function LoginFailPage() {
             className="size-28 sm:size-32"
           />
           <span className="flex flex-col items-center gap-4">
-            <h1 className=" text-4xl sm:text-6xl">로그인에 실패했습니다.😢</h1>
-            <h3 className="text-l sm:text-2xl ">
+            <h1 className="text-2xl sm:text-4xl">로그인에 실패했습니다.😢</h1>
+            <h3 className="text-l sm:text-xl ">
               아래 버튼을 눌러 다시 시도해주세요.
             </h3>
-            <h3 className="text-l sm:text-2xl ">
-              오류가 계속 반복될 경우 관리자에게 문의해주세요.
+            <h3 className="text-l sm:text-xl ">
+              오류가 반복될 경우 관리자에게 문의해주세요.
             </h3>
           </span>
           <LoginBtn />
