bce92b4e2f7207a7dd3f7be45f7762f99a739127
daejlee <savif4991@gmail.com>
2024-11-06 23:41:03 +0900

    Feat: Profile refactor progressing

--- src/components/cards/MyProfileCard.tsx
+++ src/components/cards/MyProfileCard.tsx
@@ -18,23 +18,25 @@ export default function MyProfileCard({ user }: { user: User }) {
   }, []);
   const { checked, setChecked } = useCheckedStore();
   return (
-    <div className="relative flex flex-row items-center justify-between pb-12 pt-0 lg:px-8">
-      <div className="flex flex-row items-center gap-4 lg:gap-6">
-        <ProfilePic user={user} type="myCard" />
-        <div className="flex flex-col items-start gap-2">
-          <CustomLocationModal />
-          <h2 className="text-darkblue text-xl lg:text-3xl">
-            {user.intraName}
-          </h2>
-          <p className=" text-l  lg:text-xl">{user.comment}</p>
+    <div className="flex flex-col items-center justify-center gap-2 pb-2 pt-0 md:gap-0 md:pb-4 lg:px-8">
+      <div className="flex min-h-full min-w-full flex-row items-center justify-between">
+        <div className="flex flex-row items-center gap-4 lg:gap-6">
+          <ProfilePic user={user} type="myCard" />
+          <div className="flex flex-col items-start gap-2">
+            <CustomLocationModal />
+            <h2 className="text-xl text-darkblue lg:text-3xl">
+              {user.intraName}
+            </h2>
+            <p className=" text-l  lg:text-xl">{user.comment}</p>
+          </div>
         </div>
+        <MySettingModal />
       </div>
-      <MySettingModal />
-      <div className="absolute bottom-0 right-0 flex flex-row justify-center gap-2 lg:right-10">
+      <div className="flex w-full flex-row justify-end gap-2">
         <Button
-          className="text-l border-darkblue text-darkblue h-8 w-40
-           gap-1 rounded-full border-2
-           bg-white p-2 hover:bg-gray-200  lg:h-10 lg:w-52 lg:gap-2 lg:p-4 lg:text-xl"
+          className="text-l h-8 w-40 gap-1 rounded-full
+           border-2 border-darkblue bg-white
+           p-2 text-darkblue hover:bg-gray-200  lg:h-10 lg:w-52 lg:gap-2 lg:p-4 lg:text-xl"
           onClick={() => {
             setChecked(!checked);
             localStorage.setItem("checked", JSON.stringify(!checked));
