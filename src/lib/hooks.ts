4b3a9369149d94e58be6902ac6048d6f26b2e7a6
daejlee <savif4991@gmail.com>
2024-12-07 20:51:03 +0900

    Refactor: info set apis are all asynchronous

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -24,29 +24,28 @@ export default function useInfoSet() {
         userIntraId = res.intraId;
         userDefaultGroupId = res.defaultGroupId;
       })
-      .catch((err) => console.error(err))
-      .then(() => {
-        groupApi.getAllGroups().then((res) => {
-          const updatedGroups = res.map((group) => {
-            if (group.groupId === userDefaultGroupId) {
-              return { ...group, groupName: '친구 목록' };
-            }
-            return group;
-          });
-          const sortedGroup = [...updatedGroups].sort((a, b) => a.groupId - b.groupId);
-          const defaultGroup = sortedGroup.find((group) => group.groupName === '친구 목록');
-          if (defaultGroup) {
-            sortedGroup.splice(sortedGroup.indexOf(defaultGroup), 1);
-            sortedGroup.push(defaultGroup);
+      .catch((err) => console.error(err));
+    groupApi
+      .getAllGroups()
+      .then((res) => {
+        const updatedGroups = res.map((group) => {
+          if (group.groupId === userDefaultGroupId) {
+            return { ...group, groupName: '친구 목록' };
           }
-          setGroups(sortedGroup);
-          const allMemberIds = res.flatMap((group) =>
-            group.members.map((member) => member.intraId),
-          );
-          allMemberIds.push(userIntraId);
-          setAddedMembers(allMemberIds);
+          return group;
         });
-      });
+        const sortedGroup = [...updatedGroups].sort((a, b) => a.groupId - b.groupId);
+        const defaultGroup = sortedGroup.find((group) => group.groupName === '친구 목록');
+        if (defaultGroup) {
+          sortedGroup.splice(sortedGroup.indexOf(defaultGroup), 1);
+          sortedGroup.push(defaultGroup);
+        }
+        setGroups(sortedGroup);
+        const allMemberIds = res.flatMap((group) => group.members.map((member) => member.intraId));
+        allMemberIds.push(userIntraId);
+        setAddedMembers(allMemberIds);
+      })
+      .catch((err) => console.error(err));
   }, [setAddedMembers, setGroups, setUser]);
 
   // 유저가 어드민인지 확인하고 반환합니다.
