2b6a18060c845da1cf3c3d33183f19498bc3f4a0
daejlee <savif4991@gmail.com>
2024-12-13 18:15:39 +0900

    Style: CSS refactored

--- src/components/group/GroupCardContainer.tsx
+++ src/components/group/GroupCardContainer.tsx
@@ -2,13 +2,23 @@ import ProfileCard from '@/components/cards/ProfileCard';
 import { useCheckedStore, useCheckedUsersStore } from '@/lib/stores';
 import Group from '@/types/Group';
 
+const dummyUser = {
+  agree: true,
+  comment: '십오글자십오글자십오글자십오글',
+  intraId: '1234',
+  intraName: 'RipVanWinkle',
+  location: '개포 5층 6C 테라스',
+  inCluster: true,
+  defaultGroupId: 1,
+};
+
 export default function GroupCardContainer({ curGroup }: { curGroup: Group }) {
   const { checked } = useCheckedStore();
   const { checkedUsers } = useCheckedUsersStore();
 
   return (
     <>
-      <div className="grid grid-flow-row gap-4 lg:grid-cols-2 air:grid-cols-3 3xl:grid-cols-4">
+      <div className="4xl:grid-cols-4 grid grid-flow-row gap-4 lg:grid-cols-2 air:grid-cols-3">
         {curGroup.members.map(
           (member) =>
             (!checked || member.inCluster) && (
@@ -21,11 +31,16 @@ export default function GroupCardContainer({ curGroup }: { curGroup: Group }) {
               />
             ),
         )}
+        <ProfileCard
+          user={dummyUser}
+          isEdit={curGroup.isInEdit}
+          isCheck={checkedUsers.includes(dummyUser)}
+          group={curGroup}
+        />
       </div>
       {(curGroup.members.length === 0 ||
-        (checked &&
-          !curGroup.members.filter((member) => member.inCluster).length)) && (
-          <p className="text-center text-xl">아무도 없어요.. 😢</p>
+        (checked && !curGroup.members.filter((member) => member.inCluster).length)) && (
+        <p className="text-center text-xl">아무도 없어요.. 😢</p>
       )}
     </>
   );
