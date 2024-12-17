51e919c4efa356a4b757f24d18959d69311d4694
daejlee <savif4991@gmail.com>
2024-12-17 17:43:56 +0900

    Fix: init logic fixed, after agreement refreshed

--- src/components/modals/AgreementModal.tsx
+++ src/components/modals/AgreementModal.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import {
   Accordion,
   AccordionItem,
@@ -29,21 +24,16 @@ export default function AgreementModal({
       <DialogContent className="max-w-[800px]  gap-2 text-darkblue">
         <DialogHeader className="text-m  ">
           <DialogTitle>
-            <h2 className="text-l  text-darkblue">
-              개인정보 수집 및 이용 동의서(필수)
-            </h2>
+            <h2 className="text-l  text-darkblue">개인정보 수집 및 이용 동의서(필수)</h2>
           </DialogTitle>
-          (재)이노베이션 아카데미는 『개인정보 보호법』 제15조 등 관련 법령에
-          따라 서비스 이용자의 개인정보보호를 매우 중시하며, 서비스 제공에
-          반드시 필요한 개인정보의 수집⦁이용을 위하여 귀하의 동의를 받고자
-          합니다.
+          (재)이노베이션 아카데미는 『개인정보 보호법』 제15조 등 관련 법령에 따라 서비스 이용자의
+          개인정보보호를 매우 중시하며, 서비스 제공에 반드시 필요한 개인정보의 수집⦁이용을 위하여
+          귀하의 동의를 받고자 합니다.
         </DialogHeader>
         <Accordion type="multiple" className="w-full">
           <AccordionItem value="item-1">
             <AccordionTrigger>개인정보의 수집 및 이용 목적</AccordionTrigger>
-            <AccordionContent>
-              어디있니 현재 위치 확인 서비스 제공
-            </AccordionContent>
+            <AccordionContent>어디있니 현재 위치 확인 서비스 제공</AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
             <AccordionTrigger>수집하는 개인정보 항목</AccordionTrigger>
@@ -67,9 +57,8 @@ export default function AgreementModal({
               동의 거부 권리 및 동의 거부에 따른 불이익 내용 또는 제한사항
             </AccordionTrigger>
             <AccordionContent>
-              귀하는 개인정보 수집 및 이용에 대해 동의를 거부할 권리가 있습니다.
-              필수항목에 대한 동의 거부 시 어디있니 서비스 제공이 제한됨을
-              알려드립니다.
+              귀하는 개인정보 수집 및 이용에 대해 동의를 거부할 권리가 있습니다. 필수항목에 대한
+              동의 거부 시 어디있니 서비스 제공이 제한됨을 알려드립니다.
             </AccordionContent>
           </AccordionItem>
         </Accordion>
@@ -78,8 +67,10 @@ export default function AgreementModal({
           <div className="flex flex-row gap-2">
             <Button
               onClick={() => {
-                groupApi.agreeJoin();
-                setShowModal(false);
+                groupApi.agreeJoin().then((res) => {
+                  console.log(res);
+                  router.push('/');
+                });
               }}
               size="lg"
               className="text-xl"
