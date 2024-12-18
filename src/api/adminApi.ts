8e6dc097d4fca9266f52ca759a29a71e610902b9
daejlee <savif4991@gmail.com>
2024-12-18 16:03:27 +0900

    Feat: Admin check logic with header enhanced

--- src/api/adminApi.ts
+++ src/api/adminApi.ts
@@ -3,8 +3,11 @@ import { axios } from '@/lib/Axios';
 
 const adminApi = {
   getMyStatus: async (): Promise<Admin> => {
-    const response = await axios.get('/v3/admin/status');
-    if (response.status === 403) {
+    try {
+      const response = await axios.get('/v3/admin/status');
+      return response.data;
+    } catch (error) {
+      // NOTE: 현재는 status 가져올 때 일반 유저면 403인데 이제 곧 200으로 처리될 것임.
       return {
         intraName: '',
         role: 'USER',
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,24 @@
 import Image from 'next/image';
 import HeaderMenu from '@/components/header/HeaderMenu';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import HomeLinkBtn from '@/components/buttons/HomeLinkBtn';
 
-export default function Header({ isAdmin }: { isAdmin: boolean }) {
-  const router = useRouter();
-  const [inAdminPage, setInAdminPage] = useState(false);
-
-  useEffect(() => {
-    if (router.pathname === '/admin') setInAdminPage(true);
-  }, [router, setInAdminPage]);
-
+export default function Header({
+  isAdmin,
+  isAdminPage,
+}: {
+  isAdmin: boolean;
+  isAdminPage?: boolean;
+}) {
   return (
     <header className="flex flex-row items-center justify-between p-2 pb-0 md:p-4">
       <Image
-        src={`${inAdminPage ? '/image/logo/logoAdmin.svg' : '/image/logo/logoC.svg'}`}
+        src={`${isAdminPage ? '/image/logo/logoAdmin.svg' : '/image/logo/logoC.svg'}`}
         alt="logo"
         width={200}
         height={100}
         className={`${isAdmin && 'h-[50px] w-[100px] lg:h-[100px] lg:w-[300px]'} h-[50px] w-[100px] lg:h-[100px] lg:w-[200px]`}
       />
-      {inAdminPage ? <HomeLinkBtn /> : <HeaderMenu isAdmin={isAdmin} />}
+      {isAdminPage ? <HomeLinkBtn /> : <HeaderMenu isAdmin={isAdmin} />}
     </header>
   );
 }
