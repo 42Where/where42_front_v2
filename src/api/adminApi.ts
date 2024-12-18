157f2f7f8d5bdfdf13bb788064cc0ba462989c07
daejlee <savif4991@gmail.com>
2024-12-18 16:03:27 +0900

    Feat: Admin check logic with header enhanced

--- src/api/adminApi.ts
+++ src/api/adminApi.ts
@@ -7,6 +7,7 @@ const adminApi = {
       const response = await axios.get('/v3/admin/status');
       return response.data;
     } catch (error) {
+      // NOTE: 현재는 status 가져올 때 일반 유저면 403인데 이제 곧 200으로 처리될 것임.
       return {
         intraName: '',
         role: 'USER',
