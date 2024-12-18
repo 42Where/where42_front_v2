d1e96165422be9222656b0000833274e16249e6b
daejlee <savif4991@gmail.com>
2024-12-18 16:02:51 +0900

    Style: add w-full to item

--- src/components/admin/AdminAnnouncements.tsx
+++ src/components/admin/AdminAnnouncements.tsx
@@ -4,7 +4,7 @@ import { Announcement } from '@/types/Announcement';
 export default function AdminAnnouncements({ announcements }: { announcements: Announcement[] }) {
   return (
     <div className="flex w-full flex-col items-start justify-start gap-2">
-      <div className="h-vh flex h-[70vh] flex-col overflow-auto pr-1">
+      <div className="h-vh flex h-[70vh] w-full flex-col overflow-auto pr-1">
         {announcements.length === 0
           ? '공지사항이 없습니다.'
           : announcements.map((announcement) => (
