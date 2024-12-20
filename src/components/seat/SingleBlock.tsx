aa7805db92bf50a9546ca1d0b4af6dfaea68b13d
daejlee <savif4991@gmail.com>
2024-12-20 23:42:52 +0900

    Feat: Add padding when even seats

--- src/components/seat/SingleBlock.tsx
+++ src/components/seat/SingleBlock.tsx
@@ -36,19 +36,23 @@ const dummySeats = [
     row: 'r1',
     seat: 7,
   },
+  {
+    cluster: 'c1',
+    row: 'r1',
+    seat: 8,
+  },
 ];
 
 export default function SingleBlock() {
   const oddSeats = [...dummySeats.filter((s) => s.seat % 2 !== 0)];
   const evenSeats = [...dummySeats.filter((s) => s.seat % 2 === 0)];
-  console.log(oddSeats, evenSeats, dummySeats);
 
   return (
     <div className="flex flex-row items-center justify-center gap-4">
       <h2 className="text-2xl">r1</h2>
       <div className="hidden flex-col items-center justify-center lg:flex">
         <SingleRow seats={oddSeats} />
-        <SingleRow seats={evenSeats} />
+        <SingleRow seats={evenSeats} pad={oddSeats.length === evenSeats.length} />
       </div>
       <div className="block lg:hidden">
         <SingleRow seats={dummySeats} />
--- src/components/seat/SingleRow.tsx
+++ src/components/seat/SingleRow.tsx
@@ -1,10 +1,10 @@
 import SingleSeat from '@/components/seat/SingleSeat';
 import { SeatInfo } from '@/types/Seat';
 
-export default function SingleRow({ seats }: { seats: SeatInfo[] }) {
-  console.log('SEATS: ', seats);
+export default function SingleRow({ seats, pad }: { seats: SeatInfo[]; pad?: boolean }) {
   return (
     <div className="flex flex-row items-center justify-center lg:gap-10">
+      {pad && <div className="w-[52px]" />}
       {seats.map((s) => (
         <SingleSeat key={s.seat} seatNumber={s.seat} />
       ))}
