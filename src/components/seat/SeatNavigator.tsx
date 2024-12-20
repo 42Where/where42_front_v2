ea7bf7be58f15133f8d941d13f16f024e4f32ed7
daejlee <savif4991@gmail.com>
2024-12-21 00:45:32 +0900

    Feat: Add dummy

--- src/components/seat/SeatNavigator.tsx
+++ src/components/seat/SeatNavigator.tsx
@@ -1,4 +1,15 @@
 export default function SeatNavigator() {
   // TODO: Shadcn nav 가져오기
-  return null;
+  return (
+    <nav>
+      <p>c1</p>
+      <p>c2</p>
+      <p>cx1</p>
+      <p>cx2</p>
+      <p>c3</p>
+      <p>c4</p>
+      <p>c5</p>
+      <p>c6</p>
+    </nav>
+  );
 }
