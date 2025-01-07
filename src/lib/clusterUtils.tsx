02711de1fcf750d0aabf1efbd5a5cce845c2e480
daejlee <savif4991@gmail.com>
2025-01-08 02:37:28 +0900

    Feat: exclude invalid cluster user

--- src/lib/clusterUtils.tsx
+++ src/lib/clusterUtils.tsx
@@ -111,18 +111,23 @@ export const updateClusterUser = (
   user: ActiveClusterUser,
 ): Clusters => {
   const cluster = clusters[clusterKey] as OddCluster | EvenCluster | CX1Cluster | CX2Cluster;
-  if (!cluster || !(rowKey in cluster)) throw new Error('Invalid cluster or row key');
-  const row = (clusters[clusterKey] as Record<RowName, Row>)[rowKey];
-  // 1부터 시작하므로 배열 요소와 함께 사용할 때는 -1 해야 함
-  if (index - 1 < 0 || index - 1 >= row.length) throw new Error('Index out of bounds');
+  try {
+    if (!cluster || !(rowKey in cluster)) throw new Error('Invalid cluster or row key');
+    const row = (clusters[clusterKey] as Record<RowName, Row>)[rowKey];
+    // 1부터 시작하므로 배열 요소와 함께 사용할 때는 -1 해야 함
+    if (index - 1 < 0 || index - 1 >= row.length) throw new Error('Index out of bounds');
 
-  // 불변성을 유지하며 업데이트
-  const updatedRow = row.map((item, idx) => (idx + 1 === index ? user : item));
-  return {
-    ...clusters,
-    [clusterKey]: {
-      ...clusters[clusterKey],
-      [rowKey]: updatedRow,
-    },
-  };
+    // 불변성을 유지하며 업데이트
+    const updatedRow = row.map((item, idx) => (idx + 1 === index ? user : item));
+    return {
+      ...clusters,
+      [clusterKey]: {
+        ...clusters[clusterKey],
+        [rowKey]: updatedRow,
+      },
+    };
+  } catch (e) {
+    console.error(e);
+    return clusters;
+  }
 };
