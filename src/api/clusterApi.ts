dde4b683ae17bcaa3d841b63987c8e984791a936
daejlee <savif4991@gmail.com>
2025-01-07 15:49:30 +0900

    Feat: Change API

--- src/api/clusterApi.ts
+++ src/api/clusterApi.ts
@@ -3,7 +3,7 @@ import { ClusterName, ActiveClusterUser } from '@/types/Cluster';
 
 const clusterApi = {
   getClusterUsers: async ({ cluster }: { cluster: ClusterName }): Promise<ActiveClusterUser[]> => {
-    const response = await axios.get(`/v3/cluster/${cluster}`);
+    const response = await axios.get(`/v3/location/active/${cluster}`);
     return response.data.members;
   },
 };
