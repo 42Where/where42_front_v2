1189d07165764152b11106fa7cdc0a0b6bb71077
daejlee <savif4991@gmail.com>
2025-01-18 19:53:06 +0900

    Refactor: apply react query hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Header from '@/components/header/Header';
 import MyProfileCard from '@/components/cards/MyProfileCard';
 import Groups from '@/components/group/Groups';
@@ -9,29 +7,23 @@ import { useUserStore, useGroupsStore } from '@/lib/stores';
 import AgreementModal from '@/components/modals/AgreementModal';
 import useInfoSet from '@/lib/hooks';
 import Divider from '@/components/utils/Divider';
+import CardSkeleton from '@/components/utils/CardSkeleton';
+import useAdminStatus from '@/hooks/useAdminStatus';
 
 export default function Home() {
-  const router = useRouter();
   const { user } = useUserStore();
   const { groups } = useGroupsStore();
-  const { intraId, agreement } = router.query;
-  const { isAdmin, setShowModal, showModal } = useInfoSet();
-
-  useEffect(() => {
-    if (agreement && agreement === 'false') setShowModal(true);
-    if (router.query.intraId || router.query.agreement) {
-      router.replace(router.pathname, router.pathname, { shallow: true });
-    }
-  }, [intraId, agreement, router, setShowModal]);
+  const { setShowModal, showModal } = useInfoSet();
+  const isAdmin = useAdminStatus().data?.admin;
 
   return (
     <>
       <main className="flex h-full min-h-screen w-full flex-col justify-start px-1 md:px-10">
         <AgreementModal showModal={showModal} setShowModal={setShowModal} />
-        <Header isAdmin={isAdmin} />
+        <Header isAdmin={!!isAdmin} />
         {user ? <MyProfileCard user={user} /> : <ProfileSkeleton />}
         <Divider />
-        {groups && <Groups groups={groups} />}
+        {groups.length !== 0 ? <Groups groups={groups} /> : <CardSkeleton />}
       </main>
       <Footer />
     </>
