1f3d3785445e5e867125e80902e902c81a2e441b
daejlee <savif4991@gmail.com>
2025-01-19 16:43:50 +0900

    Feat: apply query option

--- src/hooks/useMyInfo.ts
+++ src/hooks/useMyInfo.ts
@@ -1,19 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
+import { queryOptions, useQuery } from '@tanstack/react-query';
 import authApi from '@/api/authApi';
 import Cookies from 'js-cookie';
 
-// never stale, revalidated when mutated
-export default function useMyInfo() {
-  // Very idea of react useQuery is effectively a global state store.
-  // This itself is global state. User store !!
+export const queryOption = queryOptions({
+  queryKey: ['userInfo'],
+  queryFn: authApi.getMyInfo,
+  staleTime: Infinity,
+  select: (u) => {
+    Cookies.set('intraId', String(u.intraId), { domain: '.where42.kr' });
+    return u;
+  },
+});
 
-  return useQuery({
-    queryKey: ['userInfo'],
-    queryFn: authApi.getMyInfo,
-    staleTime: Infinity,
-    select: (u) => {
-      Cookies.set('intraId', String(u.intraId), { domain: '.where42.kr' });
-      return u;
-    },
-  });
+export default function useMyInfo() {
+  return useQuery(queryOption);
 }
