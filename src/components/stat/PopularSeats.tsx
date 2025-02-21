66a2502ce0f55fbe9d3347cb85be4ae66d965937
daejlee <savif4991@gmail.com>
2025-02-21 14:44:54 +0900

    test: add defensive code

--- src/components/stat/PopularSeats.tsx
+++ src/components/stat/PopularSeats.tsx
@@ -7,6 +7,9 @@ type Props = {
 };
 
 export function PopularSeats({ popularSeatsRes }: Props) {
+  console.log('PopularSeats', popularSeatsRes);
+  popularSeatsRes?.forEach((seat) => console.log('seat:', seat));
+
   return (
     <div className="flex min-h-72 w-full flex-col items-start justify-center gap-2 xl:gap-6">
       <Title title="42Seoul 인기 자리" />
@@ -15,18 +18,21 @@ export function PopularSeats({ popularSeatsRes }: Props) {
         <div className="flex w-full flex-col items-center justify-center gap-5 text-darkblue">
           {!popularSeatsRes && <PopularSeatSkeletonContainer />}
           {popularSeatsRes &&
-            popularSeatsRes.map((seat, idx) => (
-              <span
-                key={seat.seat}
-                className="flex w-[310px] flex-row items-center justify-between gap-3 md:text-lg xl:text-2xl"
-              >
-                <h2>{idx + 1}</h2>
-                <h2>{seat.seat}</h2>
-                <p className="text-[10px] text-baseblue md:text-xs xl:text-sm">
-                  {seat.usingUserCount}명/{seat.usingTimeHour}시간
-                </p>
-              </span>
-            ))}
+            popularSeatsRes.map((s, idx) => {
+              if (!s || typeof s !== 'object') return null; // 방어 코드 추가
+              return (
+                <span
+                  key={s.seat}
+                  className="flex w-[310px] flex-row items-center justify-between gap-3 md:text-lg xl:text-2xl"
+                >
+                  <h2>{idx + 1}</h2>
+                  <h2>{s.seat}</h2>
+                  <p className="text-[10px] text-baseblue md:text-xs xl:text-sm">
+                    {s.usingUserCount.toString()}명/{s.usingTimeHour.toString()}시간
+                  </p>
+                </span>
+              );
+            })}
         </div>
       </StatContainer>
     </div>
