1d2238a0d9dc98fb1dd27930926a5bb5ca95d3b1
daejlee <savif4991@gmail.com>
2025-02-21 17:04:36 +0900

    style: trim css

--- src/components/stat/MyStat.tsx
+++ src/components/stat/MyStat.tsx
@@ -12,13 +12,24 @@ export function MyStat({ intraName, myFavoriteSeatsRes }: Props) {
       <Title title={`${intraName || '.........'}님의 정보`} />
       <StatContainer>
         <SubTitle title="내가 제일 자주 앉는 자리" />
-        <div className="flex h-full w-full flex-col items-center justify-center gap-3 text-base xl:text-xl">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-3 text-base text-darkblue md:text-lg xl:text-2xl">
           {(!myFavoriteSeatsRes || myFavoriteSeatsRes.length === 0) && (
-            <p>최근 이용 기록이 없어요 😶‍🌫️</p>
+            <p className="text-baseblue">최근 이용 기록이 없어요 😶‍🌫️</p>
           )}
-          {myFavoriteSeatsRes && myFavoriteSeatsRes.length !== 0 && (
-            <h2>{myFavoriteSeatsRes[0].seat}</h2>
-          )}
+          {myFavoriteSeatsRes &&
+            myFavoriteSeatsRes.length !== 0 &&
+            myFavoriteSeatsRes.map((s, idx) => (
+              <span
+                key={s.seat}
+                className="flex w-[310px] flex-row items-center justify-between gap-3 md:text-lg xl:text-2xl"
+              >
+                <h2>{idx + 1}</h2>
+                <h2>{s.seat}</h2>
+                <p className="text-[10px] text-baseblue md:text-xs xl:text-sm">
+                  {s.usingCount}번/{s.usingTimeHour}시간
+                </p>
+              </span>
+            ))}
         </div>
       </StatContainer>
     </div>
--- src/components/stat/utils/PieSkeleton.tsx
+++ src/components/stat/utils/PieSkeleton.tsx
@@ -3,9 +3,9 @@ export function PieSkeleton({ noHeader = false }: { noHeader?: boolean }) {
     <div className="flex flex-col items-center justify-center gap-2">
       <div className="size-32 animate-pulse rounded-full bg-gray-200 xl:size-40" />
       {!noHeader && (
-        <div className="flex w-10 animate-pulse flex-row items-center justify-between gap-3 rounded-lg bg-gray-200 md:h-[28px] xl:h-[32px]" />
+        <div className="flex h-[28px] w-10 animate-pulse flex-row items-center justify-between gap-3 rounded-lg bg-gray-200 xl:h-[32px]" />
       )}
-      <div className="flex w-14 animate-pulse flex-row items-center justify-between gap-3 rounded-lg bg-gray-200 md:h-[24px] xl:h-[28px]" />
+      <div className="flex h-[24px] w-14 animate-pulse flex-row items-center justify-between gap-3 rounded-lg bg-gray-200 xl:h-[28px]" />
     </div>
   );
 }
